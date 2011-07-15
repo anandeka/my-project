c939d47d9c6ae8bd95f1f2a0ff28de7699e6f556
girishkv <girish.kv@ekaplus.com>
2011-07-15 15:12:00 +0000

    ASH added new columns.

---
+++ DBScripts/ObjectsScripts/object_script_20.sql
@@ -0,0 +1,3 @@
+ALTER TABLE ash_assay_header ADD(self_assay_ash_id VARCHAR2 (15 CHAR),cp_assay_ash_id VARCHAR2 (15 CHAR),
+pricing_assay_ash_id VARCHAR2 (15 CHAR),use_for_pricing CHAR (1 BYTE),use_for_position CHAR (1 BYTE),
+IS_DELETE CHAR (1 BYTE),PREV_PRICING_ASSAY_ASH_ID VARCHAR2 (15 Char));
