9e450225734917c593a713611519bedf20140bab
anandp <anand.hulbatte@ekaplus.com>
2011-07-27 23:03:18 +0000

    For JV and Agency settlement

---
+++ DBScripts/MasterScripts/Master_Scripts_21.sql
@@ -0,0 +1,18 @@
+Insert into AXM_ACTION_MASTER
+   (ACTION_ID, ENTITY_ID, ACTION_NAME, IS_NEW_GMR_APPLICABLE, ACTION_DESC, 
+    IS_GENERATE_DOC_APPLICABLE)
+ Values
+   ('CREATE_JVS', 'Invoice ', 'Create JV Settlement Invoice', 'N', 'JV Settlement Invoice Creation', 
+    'N');
+
+Insert into CAC_CORPORATE_ACTION_CONFIG
+   (ACTION_ID, IS_ACCRUAL_POSSIBLE, IS_ESTIMATE_POSSIBLE, EFF_DATE_FIELD, IS_DOC_APPLICABLE, 
+    GMR_STATUS_ID, SHIPMENT_STATUS, IS_AFLOAT, IS_INV_POSTING_REQD)
+ Values
+   ('CREATE_JVS', NULL, NULL, NULL, 'Y', 
+    NULL, NULL, NULL, 'Y');
+
+Insert into AKM_ACTION_REF_KEY_MASTER
+   (ACTION_KEY_ID, ACTION_KEY_DESC, VALIDATION_QUERY)
+ Values
+   ('JVSRefNo', 'JVS Ref No', 'SELECT COUNT(*) FROM   AXS_ACTION_SUMMARY axs WHERE  axs.action_ref_no = :pc_action_ref_no AND    axs.corporate_id = :pc_corporate_id');
