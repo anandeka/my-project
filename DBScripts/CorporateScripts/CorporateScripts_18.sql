3b1782d040dacf1278535cb48fed005af09941e8
girishkv <girish.kv@ekaplus.com>
2011-11-18 10:46:51 +0000

    process activity.

---
+++ DBScripts/CorporateScripts/CorporateScripts_18.sql
@@ -0,0 +1,38 @@
+ Insert into AXM_ACTION_MASTER
+   (ACTION_ID, ENTITY_ID, ACTION_NAME, IS_NEW_GMR_APPLICABLE, ACTION_DESC, 
+    IS_GENERATE_DOC_APPLICABLE)
+ Values
+   ('processActivity', 'GMR Tolling ', 'Create GMR Process Activity', 'Y', 'GMR Process Activity Creation', 
+    'N');
+
+
+Insert into CAC_CORPORATE_ACTION_CONFIG
+   (ACTION_ID, IS_ACCRUAL_POSSIBLE, IS_ESTIMATE_POSSIBLE, EFF_DATE_FIELD, IS_DOC_APPLICABLE, 
+    GMR_STATUS_ID, SHIPMENT_STATUS, IS_AFLOAT, IS_INV_POSTING_REQD)
+ Values
+   ('processActivity', 'Y', 'N', 'activityDate', 'N', 
+    NULL, NULL, NULL, 'Y');
+
+Insert into AKM_ACTION_REF_KEY_MASTER
+   (ACTION_KEY_ID, ACTION_KEY_DESC, VALIDATION_QUERY)
+ Values
+   ('ProActRefNo', 'ProAct Ref No', 'SELECT COUNT(*) FROM   AXS_ACTION_SUMMARY axs WHERE  axs.action_ref_no = :pc_action_ref_no AND    axs.corporate_id = :pc_corporate_id');
+
+
+Insert into ARF_ACTION_REF_NUMBER_FORMAT
+   (ACTION_REF_NUMBER_FORMAT_ID, ACTION_KEY_ID, CORPORATE_ID, PREFIX, MIDDLE_NO_START_VALUE, 
+    MIDDLE_NO_LAST_USED_VALUE, SUFFIX, VERSION, IS_DELETED)
+ Values
+   ('ARF-ProAct', 'ProActRefNo', '&corpId', 'PA-', 1, 
+    0, '-&corpId', 1, 'N');
+
+Insert into ARFM_ACTION_REF_NO_MAPPING
+   (ACTION_REF_NO_MAPPING_ID, CORPORATE_ID, ACTION_ID, ACTION_KEY_ID, IS_DELETED)
+ Values
+   ('ARFM-ProAct', '&corpId', 'processActivity', 'ProActRefNo', 'N');
+
+
+Insert into ERC_EXTERNAL_REF_NO_CONFIG
+   (CORPORATE_ID, EXTERNAL_REF_NO_KEY, PREFIX, MIDDLE_NO_LAST_USED_VALUE, SUFFIX)
+ Values
+   ('&corpId', 'processActivity', 'PA-', 0, '-&corpId');
