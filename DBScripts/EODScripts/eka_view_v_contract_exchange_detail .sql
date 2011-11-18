a2dcdc5fd472efe8f7c7c400f0545409d5e1bd68
suresh.gottipati <suresh.gottipati@ekaplus.com>
2011-11-18 15:21:48 +0000

    Bug Fixes

--- DBScripts/EODScripts/eka_view_v_contract_exchange_detail .sql
+++ DBScripts/EODScripts/eka_view_v_contract_exchange_detail .sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE FORCE VIEW v_contract_exchange_detail AS
+CREATE OR REPLACE FORCE VIEW v_contract_exchange_detail 
+AS
    SELECT   tt.process_id, tt.internal_contract_item_ref_no, tt.element_id,
             tt.instrument_id, dim.instrument_name, pdd.derivative_def_id,
             pdd.derivative_def_name, emt.exchange_id, emt.exchange_name
@@ -11,20 +12,21 @@ CREATE OR REPLACE FORCE VIEW v_contract_exchange_detail AS
                       pcbpd_pc_base_price_detail pcbpd,
                       ppfh_phy_price_formula_header ppfh,
                       ppfd_phy_price_formula_details ppfd,
-                      pcm_physical_contract_main   pcm
+                      pcm_physical_contract_main pcm
                 WHERE pci.pcdi_id = pcdi.pcdi_id
                   AND pcdi.pcdi_id = poch.pcdi_id
                   AND poch.poch_id = pocd.poch_id
                   AND pocd.pcbpd_id = pcbpd.pcbpd_id
                   AND pcbpd.pcbpd_id = ppfh.pcbpd_id
                   AND ppfh.ppfh_id = ppfd.ppfh_id
-                  AND pcdi.internal_contract_ref_no=pcm.internal_contract_ref_no
+                  AND pcdi.internal_contract_ref_no =
+                                                  pcm.internal_contract_ref_no
                   AND pci.process_id = pcdi.process_id
                   AND pcdi.process_id = pcbpd.process_id
                   AND pcbpd.process_id = ppfh.process_id
                   AND ppfh.process_id = ppfd.process_id
-                  and ppfd.process_id=pcm.process_id
-                  AND pcm.is_active='Y'
+                  AND ppfd.process_id = pcm.process_id
+                  AND pcm.is_active = 'Y'
                   AND pci.is_active = 'Y'
                   AND pcdi.is_active = 'Y'
                   AND poch.is_active = 'Y'
@@ -32,7 +34,7 @@ CREATE OR REPLACE FORCE VIEW v_contract_exchange_detail AS
                   AND pcbpd.is_active = 'Y'
                   AND ppfh.is_active = 'Y'
                   AND ppfd.is_active = 'Y'
-                  AND pcm.product_group_type='BASEMETAL'
+                  AND pcm.product_group_type = 'BASEMETAL'
                   AND pcdi.price_option_call_off_status IN
                                              ('Called Off', 'Not Applicable')
              GROUP BY pci.internal_contract_item_ref_no,
@@ -49,7 +51,7 @@ CREATE OR REPLACE FORCE VIEW v_contract_exchange_detail AS
                       pcbpd_pc_base_price_detail pcbpd,
                       ppfh_phy_price_formula_header ppfh,
                       ppfd_phy_price_formula_details ppfd,
-                      pcm_physical_contract_main   pcm
+                      pcm_physical_contract_main pcm
                 WHERE pci.internal_contract_item_ref_no =
                                            pcipf.internal_contract_item_ref_no
                   AND pcipf.pcbph_id = pcbph.pcbph_id
@@ -57,17 +59,18 @@ CREATE OR REPLACE FORCE VIEW v_contract_exchange_detail AS
                   AND pcbpd.pcbpd_id = ppfh.pcbpd_id
                   AND ppfh.ppfh_id = ppfd.ppfh_id
                   AND pci.pcdi_id = pcdi.pcdi_id
-                  AND pcdi.internal_contract_ref_no=pcm.internal_contract_ref_no
+                  AND pcdi.internal_contract_ref_no =
+                                                  pcm.internal_contract_ref_no
                   AND pci.process_id = pcdi.process_id
                   AND pcdi.process_id = pcipf.process_id
                   AND pcipf.process_id = pcbph.process_id
                   AND pcbph.process_id = ppfh.process_id
                   AND ppfh.process_id = ppfd.process_id
-                  AND ppfd.process_id=pcm.process_id
+                  AND ppfd.process_id = pcm.process_id
                   AND pcdi.is_active = 'Y'
-                  AND pcm.product_group_type='BASEMETAL'
+                  AND pcm.product_group_type = 'BASEMETAL'
                   AND pcdi.price_option_call_off_status = 'Not Called Off'
-                  AND pcm.is_active='Y'
+                  AND pcm.is_active = 'Y'
                   AND pci.is_active = 'Y'
                   AND pcipf.is_active = 'Y'
                   AND pcbph.is_active = 'Y'
@@ -78,51 +81,51 @@ CREATE OR REPLACE FORCE VIEW v_contract_exchange_detail AS
                       ppfd.instrument_id,
                       pcbpd.element_id,
                       pci.process_id
-              UNION ALL
-              SELECT   pci.process_id, pci.internal_contract_item_ref_no,
+             UNION ALL
+             SELECT   pci.process_id, pci.internal_contract_item_ref_no,
                       pcbpd.element_id, ppfd.instrument_id
                  FROM pci_physical_contract_item pci,
                       pcdi_pc_delivery_item pcdi,
-                      pcipf_pci_pricing_formula pcipf,
-                      pcbph_pc_base_price_header pcbph,
+                      poch_price_opt_call_off_header  poch, 
+                      pocd_price_option_calloff_dtls pocd,                    
                       pcbpd_pc_base_price_detail pcbpd,
                       ppfh_phy_price_formula_header ppfh,
                       ppfd_phy_price_formula_details ppfd,
                       dipq_delivery_item_payable_qty dipq,
-                      pcm_physical_contract_main  pcm
-                WHERE pci.internal_contract_item_ref_no =  pcipf.internal_contract_item_ref_no
-                  AND pcipf.pcbph_id = pcbph.pcbph_id
-                  AND pcbph.pcbph_id = pcbpd.pcbph_id
-                  AND pcbpd.pcbpd_id = ppfh.pcbpd_id
-                  AND ppfh.ppfh_id = ppfd.ppfh_id
-                  AND pci.pcdi_id = pcdi.pcdi_id
-                  AND pcdi.pcdi_id=dipq.pcdi_id
-                  AND pcdi.internal_contract_ref_no=pcm.internal_contract_ref_no
+                      pcm_physical_contract_main pcm
+                WHERE pci.pcdi_id=pcdi.pcdi_id
+                  AND pcdi.pcdi_id = poch.pcdi_id
+                  AND poch.poch_id = pocd.poch_id
+                  AND pocd.pcbpd_id = pcbpd.pcbpd_id
+                  AND pcbpd.pcbpd_id = ppfh.pcbpd_id  
+                  AND ppfh.ppfh_id = ppfd.ppfh_id              
+                  AND pcdi.pcdi_id = dipq.pcdi_id
+                  AND pcdi.internal_contract_ref_no =
+                                                  pcm.internal_contract_ref_no
                   AND pci.process_id = pcdi.process_id
-                  AND pcdi.process_id = pcipf.process_id
-                  AND pcipf.process_id = pcbph.process_id
-                  AND pcbph.process_id = ppfh.process_id
+                  AND pcdi.process_id = pcbpd.process_id
+                  AND pcbpd.process_id = ppfh.process_id
                   AND ppfh.process_id = ppfd.process_id
-                  AND ppfd.process_id=dipq.process_id
-                  AND dipq.process_id=pcm.process_id
+                  AND ppfd.process_id = dipq.process_id
+                  AND dipq.process_id = pcm.process_id
                   AND pcdi.is_active = 'Y'
                   AND dipq.price_option_call_off_status IN
                                              ('Called Off', 'Not Applicable')
-                  AND pcm.product_group_type='CONCENTRATES'                                             
-                  AND pcm.is_active='Y'                           
-                  AND dipq.is_active='Y'                            
-                  AND pci.is_active = 'Y'
-                  AND pcipf.is_active = 'Y'
-                  AND pcbph.is_active = 'Y'
+                  AND pcm.product_group_type = 'CONCENTRATES'
+                  AND pcm.is_active = 'Y'
+                  AND dipq.is_active = 'Y'
+                  AND pci.is_active = 'Y'                                  
                   AND pcbpd.is_active = 'Y'
+                  AND poch.is_active='Y'
+                  AND pocd.is_active='Y'
                   AND ppfh.is_active = 'Y'
-                  AND ppfd.is_active = 'Y'
+                  AND ppfd.is_active = 'Y'                  
              GROUP BY pci.internal_contract_item_ref_no,
                       ppfd.instrument_id,
                       pcbpd.element_id,
                       pci.process_id
-              UNION ALL
-              SELECT   pci.process_id, pci.internal_contract_item_ref_no,
+             UNION ALL
+             SELECT   pci.process_id, pci.internal_contract_item_ref_no,
                       pcbpd.element_id, ppfd.instrument_id
                  FROM pci_physical_contract_item pci,
                       pcdi_pc_delivery_item pcdi,
@@ -132,27 +135,29 @@ CREATE OR REPLACE FORCE VIEW v_contract_exchange_detail AS
                       ppfh_phy_price_formula_header ppfh,
                       ppfd_phy_price_formula_details ppfd,
                       dipq_delivery_item_payable_qty dipq,
-                      pcm_physical_contract_main   pcm
-                WHERE pci.internal_contract_item_ref_no =  pcipf.internal_contract_item_ref_no
+                      pcm_physical_contract_main pcm
+                WHERE pci.internal_contract_item_ref_no =
+                                           pcipf.internal_contract_item_ref_no
                   AND pcipf.pcbph_id = pcbph.pcbph_id
                   AND pcbph.pcbph_id = pcbpd.pcbph_id
                   AND pcbpd.pcbpd_id = ppfh.pcbpd_id
                   AND ppfh.ppfh_id = ppfd.ppfh_id
                   AND pci.pcdi_id = pcdi.pcdi_id
-                  AND pcdi.pcdi_id=dipq.pcdi_id
-                  AND pcdi.internal_contract_ref_no=pcm.internal_contract_ref_no
+                  AND pcdi.pcdi_id = dipq.pcdi_id
+                  AND pcdi.internal_contract_ref_no =
+                                                  pcm.internal_contract_ref_no
                   AND pci.process_id = pcdi.process_id
                   AND pcdi.process_id = pcipf.process_id
                   AND pcipf.process_id = pcbph.process_id
                   AND pcbph.process_id = ppfh.process_id
                   AND ppfh.process_id = ppfd.process_id
-                  AND ppfd.process_id=dipq.process_id
-                  AND dipq.process_id=pcm.process_id
+                  AND ppfd.process_id = dipq.process_id
+                  AND dipq.process_id = pcm.process_id
                   AND pcdi.is_active = 'Y'
                   AND dipq.price_option_call_off_status = 'Not Called Off'
-                  AND pcm.product_group_type='CONCENTRATES'
-                  AND pcm.is_active='Y' 
-                  AND dipq.is_active='Y'
+                  AND pcm.product_group_type = 'CONCENTRATES'
+                  AND pcm.is_active = 'Y'
+                  AND dipq.is_active = 'Y'
                   AND pci.is_active = 'Y'
                   AND pcipf.is_active = 'Y'
                   AND pcbph.is_active = 'Y'
