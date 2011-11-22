fc3ef1f891d796e10ae1794c2a64168ee9d97985
anupkumar.agarwal <anupkumar.agarwal@ekaplus.com>
2011-11-22 11:15:47 +0000

    Master Script :For GMC Entry Of New Weighing And Sampling From GMR Operations Object Script : For 2 New Columns Added in ASH Table To maintain Population Of WtAvgPricingAssay

---
+++ DBScripts/MasterScripts/Master_Scripts_101.sql
@@ -0,0 +1,6 @@
+Insert into GMC_GRID_MENU_CONFIGURATION
+   (MENU_ID, GRID_ID, MENU_DISPLAY_NAME, DISPLAY_SEQ_NO, MENU_LEVEL_NO, 
+    FEATURE_ID, LINK_CALLED, ICON_CLASS, MENU_PARENT_ID, ACL_ID)
+ Values
+   ('107', 'LOG', 'Weighing And Sampling', 6, 2, 
+    NULL, 'function(){loadWeighingAndSamplingMining();}', NULL, '102', NULL);
