5f24f67b1f71037b1ee11d4fac474965f53c1ade
baijukaimala <baiju.kaimala@ekaplus.com>
2011-11-29 03:53:48 +0000

    corrected

--- DBScripts/CorporateScripts/CorporateScripts_23.sql
+++ DBScripts/CorporateScripts/CorporateScripts_23.sql
@@ -1,41 +1,23 @@
 
-   
-   Insert into AKM_ACTION_REF_KEY_MASTER
-   (ACTION_KEY_ID, ACTION_KEY_DESC, VALIDATION_QUERY)
- Values
-   ('PCTSRefNo', 'Create Purchase Toll Servicee', 'SELECT COUNT(*) FROM   AXS_ACTION_SUMMARY axs WHERE  axs.action_ref_no 
-
-= :pc_action_ref_no AND    axs.corporate_id = :pc_corporate_id');
-
-
 Insert into ARF_ACTION_REF_NUMBER_FORMAT
    (ACTION_REF_NUMBER_FORMAT_ID, ACTION_KEY_ID, CORPORATE_ID, PREFIX, MIDDLE_NO_START_VALUE, 
     MIDDLE_NO_LAST_USED_VALUE, SUFFIX, VERSION, IS_DELETED)
  Values
-   ('ARF-PCTS-'||SEQ_ARF.nextval, 'PCTSRefNo', '&corporateId', 'PCT-', 1, 
-    0, '&corporateId', 1, 'N');
+   ('ARF-PCTS-'||SEQ_ARF.nextval, 'PCTSRefNo', '&corpId', 'PCT-', 1, 
+    0, '&corpId', 1, 'N');
 
 Insert into ARFM_ACTION_REF_NO_MAPPING
    (ACTION_REF_NO_MAPPING_ID, CORPORATE_ID, ACTION_ID, ACTION_KEY_ID, IS_DELETED)
  Values
    ('ARFM-PCTS-&corpId', '&corpId', 'CREATE_PURCHASE_TOLL_SERVICE', 'PCTSRefNo', 'N');  
 
-
-
-    Insert into AKM_ACTION_REF_KEY_MASTER
-   (ACTION_KEY_ID, ACTION_KEY_DESC, VALIDATION_QUERY)
- Values
-   ('SCTSRefNo', 'Create Sales Toll Servicee', 'SELECT COUNT(*) FROM   AXS_ACTION_SUMMARY axs WHERE  axs.action_ref_no 
-
-= :pc_action_ref_no AND    axs.corporate_id = :pc_corporate_id');
-
-
+   
 Insert into ARF_ACTION_REF_NUMBER_FORMAT
    (ACTION_REF_NUMBER_FORMAT_ID, ACTION_KEY_ID, CORPORATE_ID, PREFIX, MIDDLE_NO_START_VALUE, 
     MIDDLE_NO_LAST_USED_VALUE, SUFFIX, VERSION, IS_DELETED)
  Values
-   ('ARF-SCTS-'||SEQ_ARF.nextval, 'SCTSRefNo', '&corporateId', 'SCT-', 1, 
-    0, '&corporateId', 1, 'N');
+   ('ARF-SCTS-'||SEQ_ARF.nextval, 'SCTSRefNo', '&corpId', 'SCT-', 1, 
+    0, '&corpId', 1, 'N');
 
 Insert into ARFM_ACTION_REF_NO_MAPPING
    (ACTION_REF_NO_MAPPING_ID, CORPORATE_ID, ACTION_ID, ACTION_KEY_ID, IS_DELETED)
--- DBScripts/MasterScripts/Master_Scripts_105.sql
+++ DBScripts/MasterScripts/Master_Scripts_105.sql
@@ -13,3 +13,16 @@ Insert into AXM_ACTION_MASTER
  Values
    ('CREATE_SALES_TOLL_SERVICE', 'Tolling ', 'Create Sales Tolling Service', 'N', 'Create Sales Tolling Service', 
     'Y');
+   Insert into AKM_ACTION_REF_KEY_MASTER
+   (ACTION_KEY_ID, ACTION_KEY_DESC, VALIDATION_QUERY)
+ Values
+   ('PCTSRefNo', 'Create Purchase Toll Servicee', 'SELECT COUNT(*) FROM   AXS_ACTION_SUMMARY axs WHERE  axs.action_ref_no 
+
+= :pc_action_ref_no AND    axs.corporate_id = :pc_corporate_id');
+
+ Insert into AKM_ACTION_REF_KEY_MASTER
+   (ACTION_KEY_ID, ACTION_KEY_DESC, VALIDATION_QUERY)
+ Values
+   ('SCTSRefNo', 'Create Sales Toll Servicee', 'SELECT COUNT(*) FROM   AXS_ACTION_SUMMARY axs WHERE  axs.action_ref_no 
+
+= :pc_action_ref_no AND    axs.corporate_id = :pc_corporate_id');
