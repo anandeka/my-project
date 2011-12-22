df4f370b069e4091aa2d89fdb843ab3f6b8d9d2a
sivachalabathi.s <sivachalabathi.s@ekaplus.com>
2011-12-22 07:03:25 +0000

    online price calculation added

---
+++ DBScripts/ObjectsScripts/eka_view_v_gmr_exchange_details.sql
@@ -0,0 +1,57 @@
+create or replace view v_gmr_exchange_details as
+select pofh.internal_gmr_ref_no,
+       ppfd.instrument_id,
+       dim.instrument_name,
+       pdd.derivative_def_id,
+       pdd.derivative_def_name,
+       emt.exchange_id,
+       emt.exchange_name,
+       pcbpd.element_id,
+       pofh.qty_to_be_fixed,
+       pofh.pofh_id,
+       pofh.pocd_id,
+       pofh.no_of_prompt_days,
+       pofh.per_day_pricing_qty,
+       round(nvl(pofh.priced_qty, 0), 5) priced_qty,
+       round(pofh.qty_to_be_fixed - round(nvl(pofh.priced_qty, 0), 5), 5) unpriced_qty,
+       pofh.qp_start_date,
+       pofh.qp_end_date
+  from pofh_price_opt_fixation_header pofh,
+       pocd_price_option_calloff_dtls pocd,
+       pcbpd_pc_base_price_detail     pcbpd,
+       ppfh_phy_price_formula_header  ppfh,
+       ppfd_phy_price_formula_details ppfd,
+       dim_der_instrument_master      dim,
+       pdd_product_derivative_def     pdd,
+       emt_exchangemaster             emt
+ where pofh.pocd_id = pocd.pocd_id
+   and pocd.pcbpd_id = pcbpd.pcbpd_id
+   and pcbpd.pcbpd_id = ppfh.pcbpd_id
+   and ppfh.ppfh_id = ppfd.ppfh_id
+   and ppfd.instrument_id = dim.instrument_id
+   and dim.product_derivative_id = pdd.derivative_def_id
+   and pdd.exchange_id = emt.exchange_id(+)
+   and pofh.internal_gmr_ref_no is not null
+   and pofh.is_active = 'Y'
+   and pocd.is_active = 'Y'
+   and pcbpd.is_active = 'Y'
+   and ppfh.is_active = 'Y'
+   and ppfd.is_active = 'Y'
+ group by pofh.internal_gmr_ref_no,
+          ppfd.instrument_id,
+          dim.instrument_name,
+          pdd.derivative_def_id,
+          pdd.derivative_def_name,
+          emt.exchange_id,
+          emt.exchange_name,
+          pcbpd.element_id,
+          pofh.qty_to_be_fixed,
+          pofh.pofh_id,
+          pofh.pocd_id,
+          pofh.no_of_prompt_days,
+          pofh.per_day_pricing_qty,
+          pofh.priced_qty,
+          pofh.qty_to_be_fixed,
+          pofh.priced_qty,
+          pofh.qp_start_date,
+          pofh.qp_end_date
