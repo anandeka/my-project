f08da7736afc172dbbcb7ac51ee6ce7de2a5167d
mohitkumar.gupta <mohitkumar.gupta@ekaplus.com>
2011-12-22 13:03:27 +0000

    for bug 60221.

---
+++ DBScripts/MasterScripts/Master_Scripts_148.sql
@@ -0,0 +1,9 @@
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('contractApprovalStatus', 'Pending Approval', 'N', 1);
+   
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('contractApprovalStatus', 'Rejected', 'N', 2);
