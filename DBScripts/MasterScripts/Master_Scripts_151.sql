7de7eb40382b7ad07f80f7a7f0c25806dc33df4a
pradeep.kumar <pradeep.kumar@ekaplus.com>
2011-12-27 12:58:56 +0000

    -- SLV Entry for Metal Account Transaction

---
+++ DBScripts/MasterScripts/Master_Scripts_151.sql
@@ -0,0 +1,14 @@
+Insert into SLV_STATIC_LIST_VALUE
+   (VALUE_ID, VALUE_TEXT)
+ Values
+   ('Contractual Assay', 'Contractual Assay');
+
+Insert into SLV_STATIC_LIST_VALUE
+   (VALUE_ID, VALUE_TEXT)
+ Values
+   ('Provisional Assay', 'Provisional Assay');
+   
+Insert into SLV_STATIC_LIST_VALUE
+   (VALUE_ID, VALUE_TEXT)
+ Values
+   ('Final Assay', 'Final Assay');
