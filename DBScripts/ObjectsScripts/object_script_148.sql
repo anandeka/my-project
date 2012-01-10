7098bc23e9f7205749655a86edd08d2d8766b90f
girishkv <girish.kv@ekaplus.com>
2012-01-10 08:43:50 +0000

    draft invoice output DOC.

---
+++ DBScripts/ObjectsScripts/object_script_148.sql
@@ -0,0 +1,6 @@
+
+alter table IS_D add(IS_INV_DRAFT  CHAR(1 CHAR)   DEFAULT 'N');
+
+alter table API_D add(IS_INV_DRAFT  CHAR(1 CHAR)   DEFAULT 'N');
+
+alter table VAT_D add(IS_INV_DRAFT  CHAR(1 CHAR)   DEFAULT 'N');
