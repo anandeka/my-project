23b2c5a07408e3f09483fabea80925b20c53d0f8
pradeep.kumar <pradeep.kumar@ekaplus.com>
2012-01-16 13:06:13 +0000

    -- Create Pledge - GMC Script

---
+++ DBScripts/MasterScripts/Master_Scripts_168.sql
@@ -0,0 +1,7 @@
+
+Insert into GMC_GRID_MENU_CONFIGURATION
+   (MENU_ID, GRID_ID, MENU_DISPLAY_NAME, DISPLAY_SEQ_NO, MENU_LEVEL_NO, 
+    FEATURE_ID, LINK_CALLED, ICON_CLASS, MENU_PARENT_ID, ACL_ID)
+ Values
+   ('109', 'LOG', 'Create Pledge', 8, 2, 
+    NULL, 'function(){loadPledge();}', NULL, '102', NULL);
