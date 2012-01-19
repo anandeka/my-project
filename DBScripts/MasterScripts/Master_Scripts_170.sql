17ae338ae9994d9b217d7e9c611b60f9a2c33fd7
abhishek.vijay <abhishek.vijay@ekaplus.com>
2012-01-19 11:26:30 +0000

    change for price allocation and finalization

---
+++ DBScripts/MasterScripts/Master_Scripts_170.sql
@@ -0,0 +1,6 @@
+Insert into GMC_GRID_MENU_CONFIGURATION
+   (MENU_ID, GRID_ID, MENU_DISPLAY_NAME, DISPLAY_SEQ_NO, MENU_LEVEL_NO, 
+    FEATURE_ID, LINK_CALLED, ICON_CLASS, MENU_PARENT_ID, ACL_ID)
+ Values
+   ('108', 'LOG', 'Price Allocation and Price Finalization', 7, 2, 
+    NULL, 'function(){loadCreditNoteGeneralData();}', NULL, '102', NULL);
