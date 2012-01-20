d34f80b71a88bcb9fb09f8ebf779e9f90db98c52
sishir.pagada <sishir.pagada@ekaplus.com>
2012-01-20 09:44:16 +0000

    priceAllocation service key changed for GMR...

--- DBScripts/ObjectsScripts/object_script_157.sql
+++ DBScripts/ObjectsScripts/object_script_157.sql
@@ -1,4 +1,13 @@
 ALTER TABLE SAM_STOCK_ASSAY_MAPPING
  ADD (IS_PROPAGATED_ASSAY  CHAR(1 CHAR)             DEFAULT 'N');
 
-update SLS_STATIC_LIST_SETUP set IS_DEFAULT='Y' where LIST_TYPE='PriceAllocationMethod' and VALUE_ID='Weighted Average Price';
+SET DEFINE OFF;
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('GmrPriceAllocationMethod', 'Weighted Average Price', 'N', 1);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('GmrPriceAllocationMethod', 'Price Allocation', 'N', 2);
+
