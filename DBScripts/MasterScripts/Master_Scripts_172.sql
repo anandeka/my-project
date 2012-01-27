300caf93ecf1b9d2eedb662a1ac7ba1808c51a4d
abhishek.vijay <abhishek.vijay@ekaplus.com>
2012-01-27 14:11:11 +0000

    change in listing screen

---
+++ DBScripts/MasterScripts/Master_Scripts_172.sql
@@ -0,0 +1,152 @@
+SET DEFINE OFF;
+update GM_GRID_MASTER
+set GM_GRID_MASTER.DEFAULT_RECORD_MODEL_STATE='[{"dataIndex":"","fixed":true,"header":"<div class=\"x-grid3-hd-checker\">&#160;</div>","hideable":false,"id":"checker","sortable":false,"width":20},{"dataIndex":"contractNo","header":"Contract Ref. No","id":1,"sortable":true,"width":150},{"dataIndex":"tollingServiceType","header":"Tolling Service Type","id":2,"sortable":true,"width":150},{"dataIndex":"counterParty","header":"CP Name","id":3,"sortable":true,"width":150},{"dataIndex":"partnershipType","header":"Execution Type","id":4,"sortable":true,"width":150},{"dataIndex":"tradeType","header":"Product Group Type","id":5,"sortable":true,"width":150},{"dataIndex":"itemStatus","header":"Item Status","id":6,"sortable":true,"width":150},{"dataIndex":"deliveryRefNo","header":"Delivery Ref. No.","id":7,"sortable":true,"width":150},{"dataIndex":"itemRefNo","header":"Item Ref. No.","id":8,"sortable":true,"width":150},{"dataIndex":"product","header":"Product","id":9,"sortable":true,"width":150},{"dataIndex":"quality","header":"Quality","id":10,"sortable":true,"width":150},{"dataIndex":"issueDate","header":"Issue Date","id":11,"sortable":true,"width":150},{"dataIndex":"quotaMonth","header":"Quota Period","id":12,"sortable":true,"width":150},{"dataIndex":"location","header":"Location","id":13,"sortable":true,"width":150},{"dataIndex":"traxysOrg","header":"Org","id":14,"sortable":true,"width":150},{"dataIndex":"incotermLocation","header":"INCO Term & Location","id":15,"sortable":true,"width":150},{"dataIndex":"qty","header":"Item Quantity","id":16,"sortable":true,"width":150},{"dataIndex":"openQty","header":"Open Qty.","id":17,"sortable":true,"width":150},{"dataIndex":"allocatedQty","header":"Allocated Qty.","id":18,"sortable":true,"width":150},{"dataIndex":"incoterm","header":"Incoterm","id":19,"sortable":true,"width":150},{"dataIndex":"strategy","header":"Strategy","id":20,"sortable":true,"width":150},{"dataIndex":"bookProfitCenter","header":"Book/Profit Center","id":21,"sortable":true,"width":150},{"dataIndex":"trader","header":"Trader","id":22,"sortable":true,"width":150},{"dataIndex":"pricing","header":"Pricing","id":23,"sortable":true,"width":150},{"dataIndex":"executedQty","header":"Executed Quantity","id":24,"sortable":true,"width":150},{"dataIndex":"titleTransferQty","header":"Title Transfer Quantity","id":25,"sortable":true,"width":150},{"dataIndex":"provInvoicedQty","header":"Prov. Invoiced Quantity","id":26,"sortable":true,"width":150},{"dataIndex":"finalInvoicedQty","header":"Final Invoiced Quantity","id":27,"sortable":true,"width":150},{"dataIndex":"payInCurrency","header":"Pay-In Currency","id":28,"sortable":true,"width":150},{"dataIndex":"payableContent","header":"Payable Content","id":29,"sortable":true,"width":150},{"dataIndex":"origin","header":"Origin","id":30,"sortable":true,"width":150},{"dataIndex":"passThrough","header":"passThrough","id":31,"sortable":true,"width":150}]'
+where GM_GRID_MASTER.GRID_ID='MLOCI';
+
+
+
+
+
+SET DEFINE OFF;
+update GM_GRID_MASTER
+set GM_GRID_MASTER.DEFAULT_RECORD_MODEL_STATE='[{"dataIndex":"","fixed":true,"header":"<div class=\"x-grid3-hd-checker\">&#160;</div>","hideable":false,"id":"checker","sortable":false,"width":20},{"dataIndex":"contractRefNo","header":"Contract No.","id":1,"sortable":true,"width":150},
+{"dataIndex":"tollingServiceType","header":"Tolling Service Type","id":2,"sortable":true,"width":150},
+{"dataIndex":"cpName","header":"CP Name","id":3,"sortable":true,"width":150},
+{"dataIndex":"tradeType","header":"Execution Type","id":4,"sortable":true,"width":150},
+{"dataIndex":"deliveryItemRefNo","header":"Delivery Ref. No.","id":5,"sortable":true,"width":150},
+{"dataIndex":"assetclass","header":"Product","id":6,"sortable":true,"width":150},
+{"dataIndex":"qualityName","header":"Quality","id":7,"sortable":true,"width":150},
+{"dataIndex":"attributes","header":"Attributes","id":8,"sortable":true,"width":150},
+{"dataIndex":"issueDate","header":"Issue Date","id":9,"sortable":true,"width":150},
+{"dataIndex":"quotaMonth","header":"Quota Period","id":10,"sortable":true,"width":150},
+{"dataIndex":"location","header":"Location","id":11,"sortable":true,"width":150},
+{"dataIndex":"traxysOrg","header":"Org","id":12,"sortable":true,"width":150},
+{"dataIndex":"incotermLocation","header":"Incoterm & Location","id":13,"sortable":true,"width":150},
+{"dataIndex":"quotaQty","header":"Quota Quantity(Min)","id":14,"sortable":true,"width":150},
+{"dataIndex":"quotaQtyUnit","header":"Quota Qty. Unit","id":15,"sortable":true,"width":150},
+{"dataIndex":"quotaOpenQty","header":"Quota Open Qty.","id":16,"sortable":true,"width":150},
+{"dataIndex":"allocatedQty","header":"Allocated Qty.","id":17,"sortable":true,"width":150},
+{"dataIndex":"strategy","header":"Strategy","id":18,"sortable":true,"width":150},
+{"dataIndex":"bookProfitCenter","header":"Book/Profit Center","id":19,"sortable":true,"width":150},
+{"dataIndex":"trader","header":"Trader","id":20,"sortable":true,"width":150},
+{"dataIndex":"qpPricingBasis","header":"QP-Pricing Basis","id":21,"sortable":true,"width":150},
+{"dataIndex":"qpPricing","header":"QP-Pricing","id":22,"sortable":true,"width":150},
+{"dataIndex":"pricing","header":"Pricing","id":23,"sortable":true,"width":150},
+{"dataIndex":"quotaQuantityMax","header":"Quota Quantity(Max)","id":24,"sortable":true,"width":150},
+{"dataIndex":"quotaQuantityBasis","header":"Quota Quantity Basis","id":25,"sortable":true,"width":150},
+{"dataIndex":"toBeCalledOffQty","header":"To be called Off Quantity","id":26,"sortable":true,"width":150},
+{"dataIndex":"calledOffQty","header":"Called Off Quantity","id":27,"sortable":true,"width":150},
+{"dataIndex":"executedQty","header":"Executed Quantity","id":28,"sortable":true,"width":150},
+{"dataIndex":"pricingStatus","header":"Pricing Status","id":29,"sortable":true,"width":150},
+{"dataIndex":"fixedPriceQty","header":"Fixed Price Quantity","id":30,"sortable":true,"width":150},
+{"dataIndex":"titleTransferQty","header":"Title Transfer Quantity","id":31,"sortable":true,"width":150},
+{"dataIndex":"provInvoicedQty","header":"Prov. Invoiced Quantity","id":32,"sortable":true,"width":150},
+{"dataIndex":"finalInvoicedQty","header":"Final Invoiced Quantity","id":33,"sortable":true,"width":150},
+{"dataIndex":"payInCurrency","header":"Pay-In Currency","id":34,"sortable":true,"width":150},
+{"dataIndex":"fullfillmentStatus","header":"Fullfillment Status","id":35,"sortable":true,"width":150},
+{"dataIndex":"passThrough","header":"Pass Through","id":36,"sortable":true,"width":150}]'
+where GM_GRID_MASTER.GRID_ID='MLODI';
+
+
+
+
+
+SET DEFINE OFF;
+update GM_GRID_MASTER
+set GM_GRID_MASTER.DEFAULT_RECORD_MODEL_STATE='[   
+                              {header: "Contract Ref No.", width: 150, sortable: true,renderer:contractOrderStatus, dataIndex: "contractRefNo"},
+                                 {header: "Issue Date", width: 150, sortable: true, dataIndex: "issueDate"},
+                                 {header: "CP Name", width: 150, sortable: true, dataIndex: "cpName"},
+                                 {header: "Tolling Serivce Type", width: 150, sortable: true, dataIndex: "tollingServiceType"},
+                                 
+                                 {header: "Product ", width: 150, sortable: true, dataIndex: "assetclass"},
+                                 {header: "Product Type ", width: 150, sortable: true, dataIndex: "productGroupType"},
+                                 {header: "No. Of. Delivery Items", width: 150, sortable: true, dataIndex: "noOfItems"},
+                                 {header: "Contract Quanity", width: 150, sortable: true, dataIndex: "contractqty"},
+                                 {header: "Pass Through", width: 150, sortable: true, dataIndex: "passThrough"},
+                                 {header: "Contract Status", width: 150, sortable: true, dataIndex: "contractStatus"}
+                              ]'
+                              where GM_GRID_MASTER.GRID_ID='MIN_LOC';
+
+
+
+
+
+
+SET DEFINE OFF
+update GM_GRID_MASTER
+set GM_GRID_MASTER.DEFAULT_COLUMN_MODEL_STATE='[{"dataIndex":"","fixed":true,"header":"<div class=\"x-grid3-hd-checker\">&#160;</div>","hideable":false,"id":"checker","sortable":false,"width":20},{"dataIndex":"contractNo","header":"Contract Ref. No","id":1,"sortable":true,"width":150},{"dataIndex":"tollingServiceType","header":"Tolling Serivce Type","id":2,"sortable":true,"width":150},{"dataIndex":"counterParty","header":"CP Name","id":3,"sortable":true,"width":150},{"dataIndex":"partnershipType","header":"Execution Type","id":4,"sortable":true,"width":150},{"dataIndex":"tradeType","header":"Product Group Type","id":5,"sortable":true,"width":150},{"dataIndex":"itemStatus","header":"Item Status","id":6,"sortable":true,"width":150},{"dataIndex":"deliveryRefNo","header":"Delivery Ref. No.","id":7,"sortable":true,"width":150},{"dataIndex":"itemRefNo","header":"Item Ref. No.","id":8,"sortable":true,"width":150},{"dataIndex":"product","header":"Product","id":9,"sortable":true,"width":150},{"dataIndex":"quality","header":"Quality","id":10,"sortable":true,"width":150},{"dataIndex":"issueDate","header":"Issue Date","id":11,"sortable":true,"width":150},{"dataIndex":"quotaMonth","header":"Quota Period","id":12,"sortable":true,"width":150},{"dataIndex":"location","header":"Location","id":13,"sortable":true,"width":150},{"dataIndex":"traxysOrg","header":"Org","id":14,"sortable":true,"width":150},{"dataIndex":"incotermLocation","header":"INCO Term & Location","id":15,"sortable":true,"width":150},{"dataIndex":"qty","header":"Item Quantity","id":16,"sortable":true,"width":150},{"dataIndex":"openQty","header":"Open Qty.","id":17,"sortable":true,"width":150},{"dataIndex":"allocatedQty","header":"Allocated Qty.","id":18,"sortable":true,"width":150},{"dataIndex":"incoterm","header":"Incoterm","id":19,"sortable":true,"width":150},{"dataIndex":"strategy","header":"Strategy","id":20,"sortable":true,"width":150},{"dataIndex":"bookProfitCenter","header":"Book/Profit Center","id":21,"sortable":true,"width":150},{"dataIndex":"trader","header":"Trader","id":22,"sortable":true,"width":150},{"dataIndex":"pricing","header":"Pricing","id":23,"sortable":true,"width":150},{"dataIndex":"executedQty","header":"Executed Quantity","id":24,"sortable":true,"width":150},{"dataIndex":"titleTransferQty","header":"Title Transfer Quantity","id":25,"sortable":true,"width":150},{"dataIndex":"provInvoicedQty","header":"Prov. Invoiced Quantity","id":26,"sortable":true,"width":150},{"dataIndex":"finalInvoicedQty","header":"Final Invoiced Quantity","id":27,"sortable":true,"width":150},{"dataIndex":"payInCurrency","header":"Pay-In Currency","id":28,"sortable":true,"width":150},{"dataIndex":"payableContent","header":"Payable Content","id":29,"sortable":true,"width":150},{"dataIndex":"origin","header":"Origin","id":30,"sortable":true,"width":150},{"dataIndex":"passThrough","header":"passThrough","id":31,"sortable":true,"width":150}]'
+where GM_GRID_MASTER.GRID_ID='MLOCI';
+
+
+
+
+
+
+SET DEFINE OFF
+update GM_GRID_MASTER
+set GM_GRID_MASTER.DEFAULT_COLUMN_MODEL_STATE='[{"dataIndex":"","fixed":true,"header":"<div class=\"x-grid3-hd-checker\">&#160;</div>","hideable":false,"id":"checker","sortable":false,"width":20},{"dataIndex":"contractRefNo","header":"Contract No.","id":1,"sortable":true,"width":150},
+{"dataIndex":"tollingServiceType","header":"Tolling Service Type","id":2,"sortable":true,"width":150},
+{"dataIndex":"cpName","header":"CP Name","id":3,"sortable":true,"width":150},
+{"dataIndex":"tradeType","header":"Execution Type","id":4,"sortable":true,"width":150},
+{"dataIndex":"deliveryItemRefNo","header":"Delivery Ref. No.","id":5,"sortable":true,"width":150},
+{"dataIndex":"assetclass","header":"Product","id":6,"sortable":true,"width":150},
+{"dataIndex":"qualityName","header":"Quality","id":7,"sortable":true,"width":150},
+{"dataIndex":"attributes","header":"Attributes","id":8,"sortable":true,"width":150},
+{"dataIndex":"issueDate","header":"Issue Date","id":9,"sortable":true,"width":150},
+{"dataIndex":"quotaMonth","header":"Quota Period","id":10,"sortable":true,"width":150},
+{"dataIndex":"location","header":"Location","id":11,"sortable":true,"width":150},
+{"dataIndex":"traxysOrg","header":"Org","id":12,"sortable":true,"width":150},
+{"dataIndex":"incotermLocation","header":"Incoterm & Location","id":13,"sortable":true,"width":150},
+{"dataIndex":"quotaQty","header":"Quota Quantity(Min)","id":14,"sortable":true,"width":150},
+{"dataIndex":"quotaQtyUnit","header":"Quota Qty. Unit","id":15,"sortable":true,"width":150},
+{"dataIndex":"quotaOpenQty","header":"Quota Open Qty.","id":16,"sortable":true,"width":150},
+{"dataIndex":"allocatedQty","header":"Allocated Qty.","id":17,"sortable":true,"width":150},
+{"dataIndex":"strategy","header":"Strategy","id":18,"sortable":true,"width":150},
+{"dataIndex":"bookProfitCenter","header":"Book/Profit Center","id":19,"sortable":true,"width":150},
+{"dataIndex":"trader","header":"Trader","id":20,"sortable":true,"width":150},
+{"dataIndex":"qpPricingBasis","header":"QP-Pricing Basis","id":21,"sortable":true,"width":150},
+{"dataIndex":"qpPricing","header":"QP-Pricing","id":22,"sortable":true,"width":150},
+{"dataIndex":"pricing","header":"Pricing","id":23,"sortable":true,"width":150},
+{"dataIndex":"quotaQuantityMax","header":"Quota Quantity(Max)","id":24,"sortable":true,"width":150},
+{"dataIndex":"quotaQuantityBasis","header":"Quota Quantity Basis","id":25,"sortable":true,"width":150},
+{"dataIndex":"toBeCalledOffQty","header":"To be called Off Quantity","id":26,"sortable":true,"width":150},
+{"dataIndex":"calledOffQty","header":"Called Off Quantity","id":27,"sortable":true,"width":150},
+{"dataIndex":"executedQty","header":"Executed Quantity","id":28,"sortable":true,"width":150},
+{"dataIndex":"pricingStatus","header":"Pricing Status","id":29,"sortable":true,"width":150},
+{"dataIndex":"fixedPriceQty","header":"Fixed Price Quantity","id":30,"sortable":true,"width":150},
+{"dataIndex":"titleTransferQty","header":"Title Transfer Quantity","id":31,"sortable":true,"width":150},
+{"dataIndex":"provInvoicedQty","header":"Prov. Invoiced Quantity","id":32,"sortable":true,"width":150},
+{"dataIndex":"finalInvoicedQty","header":"Final Invoiced Quantity","id":33,"sortable":true,"width":150},
+{"dataIndex":"payInCurrency","header":"Pay-In Currency","id":34,"sortable":true,"width":150},
+{"dataIndex":"fullfillmentStatus","header":"Fullfillment Status","id":35,"sortable":true,"width":150},
+{"dataIndex":"passThrough","header":"Pass Through","id":36,"sortable":true,"width":150}]'
+where GM_GRID_MASTER.GRID_ID='MLODI';
+
+
+
+
+
+update GM_GRID_MASTER
+set GM_GRID_MASTER.DEFAULT_COLUMN_MODEL_STATE='[{"dataIndex":"","fixed":true,"header":"<div class=\"x-grid3-hd-checker\">&#160;</div>","hideable":false,"id":"checker","sortable":false,"width":20},
+{"dataIndex":"contractRefNo","header":"Contract Ref No.","id":1,"sortable":true,"width":150},
+{"dataIndex":"issueDate","header":"Issue Date","id":2,"sortable":true,"width":150},
+{"dataIndex":"cpName","header":"CP Name","id":3,"sortable":true,"width":150},
+{"dataIndex":"tollingServiceType","header":"Tolling Service Type","id":4,"sortable":true,"width":150},
+{"dataIndex":"assetclass","header":"Product ","id":5,"sortable":true,"width":150},
+{"dataIndex":"productGroupType","header":"Product Type ","id":6,"sortable":true,"width":150},
+{"dataIndex":"noOfItems","header":"No. Of. Delivery Items","id":7,"sortable":true,"width":150},
+{"dataIndex":"contractqty","header":"Contract Quanity","id":8,"sortable":true,"width":150},
+{"dataIndex":"contractStatus","header":"Contract Status","id":9,"sortable":true,"width":150},
+{"dataIndex":"org","header":"Org.","id":10,"sortable":true,"width":150},
+{"dataIndex":"strategy","header":"Strategy","id":11,"sortable":true,"width":150},
+{"dataIndex":"trader","header":"Trader","id":12,"sortable":true,"width":150},
+{"dataIndex":"executedQty","header":"Executed Quantity","id":13,"sortable":true,"width":150},
+{"dataIndex":"allocatedQty","header":"Allocated Quantity","id":14,"sortable":true,"width":150},
+{"dataIndex":"passThrough","header":"Pass Through","id":15,"sortable":true,"width":150},
+{"dataIndex":"provInvoiceQty","header":"Prov. Invoice Quantity","id":16,"sortable":true,"width":150},
+{"dataIndex":"finalInvoiceQty","header":"Final Invoice Quantity","id":17,"sortable":true,"width":150},
+{"dataIndex":"titleTransferQty","header":"Title Transfer Quantity","id":18,"sortable":true,"width":150},
+{"dataIndex":"payInCurrency","header":"Pay-In Currency","id":19,"sortable":true,"width":150}]'
+where GM_GRID_MASTER.GRID_ID='MIN_LOC';
+
