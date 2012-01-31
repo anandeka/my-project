dd6077fdf30777ef5608c3229c14410d13c86fb8
rajeshwar.reddy <rajeshwar.reddy@ekaplus.com>
2012-01-31 09:37:46 +0000

    chargeUnit

---
+++ DBScripts/MasterScripts/Master_Scripts_180.sql
@@ -0,0 +1,12 @@
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('chargeUnit', 'Dry', 'N', 1);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('chargeUnit', 'Wet', 'N', 2);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('chargeUnit', 'Lot', 'N', 3);
