25fb723f872184321b005cdca273d739fd4ea041
suresh.gottipati <suresh.gottipati@ekaplus.com>
2012-01-31 09:05:40 +0000

    Bug Fixes

--- DBScripts/ObjectsScripts/eka_view_V_AK_CORPORATE.sql
+++ DBScripts/ObjectsScripts/eka_view_V_AK_CORPORATE.sql
@@ -1,33 +1,57 @@
-DROP VIEW V_AK_CORPORATE
-/
-
---/* Formatted on 2011/08/08 15:24 (Formatter Plus v4.8.8) */
-CREATE OR REPLACE VIEW v_ak_corporate 
-AS
-   SELECT akc.corporate_id, akc.corporate_name, pad.address address1,
-          NULL address2, cym.country_name country, cim.city_name city,
-          pad.phone phone_no, pad.fax fax_no, sm.state_code,
-          sm.state_name state, pad.website website, NULL logo_name,
-          NULL logo_path, NULL logo, akc.corp_display_name1,
-          akc.corp_display_name2
-     FROM ak_corporate akc,
-          phd_profileheaderdetails phd,
-          pad_profile_addresses pad,
-          cim_citymaster cim,
-          cym_countrymaster cym,
-          sm_state_master sm
-    WHERE akc.corporate_id = phd.corporate_id
-      AND phd.isinternalcompany = 'Y'
-      AND phd.profileid = pad.profile_id
-      AND pad.address_type = 'Main'
-      AND pad.country_id = cym.country_id(+)
-      AND pad.city_id = cim.city_id(+)
-      AND pad.is_deleted = 'N'
-      AND pad.is_default = 'Y'
-      --AND akc.corporate_id = cld.corporate_id(+)
-   --   AND 'N' = cld.is_deleted(+)
-      AND pad.state_id = sm.state_id(+)
-      AND phd.is_active = 'Y'
-      AND phd.is_deleted = 'N'
-/
-
+create or replace view v_ak_corporate as
+select akc.corporate_id,
+       akc.corporate_name,
+       pad.address address1,
+       null address2,
+       null address3,
+       null address4,
+       -- cim.city_name city1,
+       -- cym.country_name country1,
+       -- cm.cur_name base_currency_name,
+       null           city1,
+       null           country1,
+       cm.cur_name    base_currency_name,
+       null           abn_number,
+       rm.region_name region,
+       --       cym.country_name country,
+       --       cim.city_name city,
+       null country,
+       null city,
+       pad.phone phone_no,
+       pad.fax fax_no,
+       pad.email,
+       sm.state_code,
+       sm.state_name state,
+       pad.website website,
+       null logo_name,
+       akl.corporate_image logo_path,
+       null logo,
+       akc.corp_display_name1,
+       akc.corp_display_name2,
+       pad.zip,
+       akl.footeraddress,
+       akl.headeraddress
+  from ak_corporate             akc,
+       phd_profileheaderdetails phd,
+       pad_profile_addresses    pad,
+       cim_citymaster           cim,
+       cym_countrymaster        cym,
+       --cld_corporate_logo_detail cld,
+       sm_state_master    sm,
+       cm_currency_master cm,
+       rem_region_master  rm,
+       ak_corporate_logo  akl
+ where akc.corporate_id = phd.corporate_id
+   and phd.profileid = pad.profile_id(+)
+   and pad.address_type(+) = 'Main'
+   and pad.country_id = cym.country_id(+)
+   and pad.city_id = cim.city_id(+)
+   and pad.is_deleted(+) = 'N'
+   and pad.is_default(+) = 'Y'
+   and akc.corporate_id = akl.corporate_id(+)
+   and pad.state_id = sm.state_id(+)
+   and cym.region_id = rm.region_id(+)
+   and phd.is_active = 'Y'
+   and phd.isinternalcompany = 'Y'
+   and phd.is_deleted = 'N'
+   and akc.base_cur_id = cm.cur_id;
