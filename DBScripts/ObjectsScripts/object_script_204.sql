270baebf0b4646c4875513b4cc79527a696c40a2
anupkumar.agarwal <anupkumar.agarwal@ekaplus.com>
2012-02-27 13:27:55 +0000

    Added Constraints

---
+++ DBScripts/ObjectsScripts/object_script_204.sql
@@ -0,0 +1,11 @@
+ALTER TABLE GRD_GOODS_RECORD_DETAIL DROP CONSTRAINT CHK_GRD_TOLLING_STOCK_TYPE;
+ 
+ALTER TABLE GRD_GOODS_RECORD_DETAIL ADD (
+  CONSTRAINT CHK_GRD_TOLLING_STOCK_TYPE CHECK (TOLLING_STOCK_TYPE IN ('None Tolling','MFT In Process Stock','Delta MFT IP Stock','Commercial Fee Stock','RM In Process Stock','RM Out Process Stock','Process Activity','Clone Stock','Input Process','Output Process','Free Material Stock','Pledge Stock','Financial Settlement Stock'))
+);
+
+ALTER TABLE AGRD_ACTION_GRD DROP CONSTRAINT CHK_AGRD_TOLLING_STOCK_TYPE;
+ 
+ALTER TABLE AGRD_ACTION_GRD ADD (
+  CONSTRAINT CHK_AGRD_TOLLING_STOCK_TYPE CHECK (TOLLING_STOCK_TYPE IN ('None Tolling','MFT In Process Stock','Delta MFT IP Stock','Commercial Fee Stock','RM In Process Stock','RM Out Process Stock','Process Activity','Clone Stock','Input Process','Output Process','Free Material Stock','Pledge Stock','Financial Settlement Stock'))
+);
