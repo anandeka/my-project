270ae8a9044056c7692274495e183eda00bafa65
anupkumar.agarwal <anupkumar.agarwal@ekaplus.com>
2012-02-28 14:07:24 +0000

    Added SLS SLV Entry Form Metal Account Transaction Activity Type Drop down

---
+++ DBScripts/MasterScripts/Master_Scripts_205.sql
@@ -0,0 +1,70 @@
+
+Insert into SLV_STATIC_LIST_VALUE
+   (VALUE_ID, VALUE_TEXT)
+ Values
+   ('pledgeTransfer', 'Pledge Transfer');
+   
+Insert into SLV_STATIC_LIST_VALUE
+   (VALUE_ID, VALUE_TEXT)
+ Values
+   ('financialSettlement', 'Financial Settlement');
+   
+Insert into SLV_STATIC_LIST_VALUE
+   (VALUE_ID, VALUE_TEXT)
+ Values
+   ('metalBalanceTransfer', 'Metal Balance Transfer');
+
+Insert into SLV_STATIC_LIST_VALUE
+   (VALUE_ID, VALUE_TEXT)
+ Values
+   ('metalBalanceTransfer', 'Metal Balance Transfer'); 
+    
+Insert into SLV_STATIC_LIST_VALUE
+   (VALUE_ID, VALUE_TEXT)
+ Values
+   ('metalBalanceTransfer', 'Metal Balance Transfer');
+        
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('MetalTransactionActivity', 'shipmentDetail', 'N', 1);
+
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('MetalTransactionActivity', 'railDetail', 'N', 2);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('MetalTransactionActivity', 'truckDetail', 'N', 3);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('MetalTransactionActivity', 'airDetail', 'N', 4);
+
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('MetalTransactionActivity', 'landingDetail', 'N', 5);
+   
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('MetalTransactionActivity', 'warehouseReceipt', 'N', 6);
+   
+   Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('MetalTransactionActivity', 'pledgeTransfer', 'N', 7);
+   
+   Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('MetalTransactionActivity', 'financialSettlement', 'N', 8);
+   
+    Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('MetalTransactionActivity', 'metalBalanceTransfer', 'N', 9);
+   
+   
