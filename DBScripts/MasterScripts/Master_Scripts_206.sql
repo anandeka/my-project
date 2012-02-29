2ac52a4b7c1f05e969b11bbb90db08f9cb8d053b
abhishek.vijay <abhishek.vijay@ekaplus.com>
2012-02-29 05:28:09 +0000

    speed pricing

---
+++ DBScripts/MasterScripts/Master_Scripts_206.sql
@@ -0,0 +1,8 @@
+
+Insert into GMC_GRID_MENU_CONFIGURATION
+   (MENU_ID, GRID_ID, MENU_DISPLAY_NAME, DISPLAY_SEQ_NO, MENU_LEVEL_NO, 
+    FEATURE_ID, LINK_CALLED, ICON_CLASS, MENU_PARENT_ID, ACL_ID)
+ Values
+   ('110', 'LOG', 'Speed Pricing', 9, 2, 
+    NULL, 'function(){loadGmrDataforSpeedPricing();}', NULL, '102', NULL);
+
