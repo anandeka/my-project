abf8f8741ebe2f9727e251e78d5ee37bb41a16dc
anupkumar.agarwal <anupkumar.agarwal@ekaplus.com>
2012-03-05 06:52:08 +0000

    Added New SLS SLV Entry For GMr status On Air

---
+++ DBScripts/MasterScripts/Master_Scripts_215.sql
@@ -0,0 +1,12 @@
+
+Insert into SLV_STATIC_LIST_VALUE
+   (VALUE_ID, VALUE_TEXT)
+ Values
+   ('On Air', 'On Air');
+
+
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('GMRListingStatus', 'On Air', 'N', '7');
+
