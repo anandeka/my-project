fb7e1c0056035712e3137aa6ba2ddb1e7fade28a
suresh.gottipati <suresh.gottipati@ekaplus.com>
2012-03-15 07:10:00 +0000

    Bug Fixes

--- DBScripts/ObjectsScripts/eka_view_v_dqd_derivative_quote_detail.sql
+++ DBScripts/ObjectsScripts/eka_view_v_dqd_derivative_quote_detail.sql
@@ -1,4 +1,4 @@
-create or replace view v_dqd_derivative_quote_detail as
+CREATE OR REPLACE  VIEW v_dqd_derivative_quote_detail as
 select dqd_id,
        dq_id,
        dr_id,
@@ -19,10 +19,39 @@ select dqd_id,
        spot_rate,
        is_manual
   from dqd_derivative_quote_detail dqd
- where dqd.dr_id not in(select drm.dr_id
-          from v_drm_multiple_prompt drm)
-
+ where dqd.dr_id not in (select drm.dr_id from v_drm_multiple_prompt drm)
 union
+select dqd.dqd_id dqd_id,
+       dqd.dq_id,
+       dqd.dr_id dr_id,
+       dqd.available_price_id,
+       dqd.price,
+       dqd.price_unit_id,
+       dqd.delta,
+       dqd.gamma,
+       dqd.theta,
+       dqd.wega,
+       dqd.is_deleted,
+       dqd.charm,
+       dqd.lambda,
+       dqd.rho,
+       dqd.volatility,
+       dqd.riskfree_rate,
+       dqd.interest_rate,
+       dqd.spot_rate,
+       dqd.is_manual
+  from dq_derivative_quotes        dq,
+       drm_derivative_master       drm,
+       v_drm_multiple_prompt       new_drm,
+       dqd_derivative_quote_detail dqd
+ where dq.dq_id = dqd.dq_id
+   and dqd.dr_id = drm.dr_id
+   and drm.instrument_id = new_drm.instrument_id
+   and drm.prompt_date = new_drm.prompt_date
+   and drm.dr_id = new_drm.dr_id
+--  and new_drm.prompt_date = '21-Apr-2010'
+--   and new_drm.instrument_id = 'DIM-11'
+union all
 select dqd.dqd_id || new_drm.dr_id dqd_id,
        dqd.dq_id,
        new_drm.dr_id dr_id,
@@ -51,3 +80,5 @@ select dqd.dqd_id || new_drm.dr_id dqd_id,
    and drm.instrument_id = new_drm.instrument_id
    and drm.prompt_date = new_drm.prompt_date
    and drm.dr_id <> new_drm.dr_id
+--and new_drm.prompt_date = '21-Apr-2010'
+--and new_drm.instrument_id = 'DIM-11'
--- DBScripts/ObjectsScripts/eka_view_v_gmr_sac_qty.sql
+++ DBScripts/ObjectsScripts/eka_view_v_gmr_sac_qty.sql
@@ -1,14 +1,20 @@
-create or replace view v_gmr_sac_qty as
-select internal_gmr_ref_no,
-       total_qty_in_wet,
-       total_qty_in_dry,
-       grd_qty_unit_id
-  from (select sac.internal_gmr_ref_no,
-               sum(sac.total_qty_in_wet) over(partition by sac.internal_gmr_ref_no order by sac.internal_gmr_ref_no) total_qty_in_wet,
-               sum(sac.total_qty_in_dry) over(partition by sac.internal_gmr_ref_no order by sac.internal_gmr_ref_no) total_qty_in_dry,
-               sac.grd_qty_unit_id
-          from sac_stock_assay_content sac)
- group by internal_gmr_ref_no,
-          total_qty_in_wet,
-          total_qty_in_dry,
-          grd_qty_unit_id
+CREATE OR REPLACE VIEW v_gmr_sac_qty 
+AS
+SELECT   t.internal_gmr_ref_no, SUM (t.total_qty_in_wet) total_qty_in_wet,
+         SUM (t.total_qty_in_dry) total_qty_in_dry,
+         SUM (t.current_qty_dry) current_qty_dry,
+         SUM (t.current_qty_wet) current_qty_wet, t.grd_qty_unit_id
+    FROM (SELECT   sac.internal_gmr_ref_no, sac.internal_grd_ref_no,
+                   sac.total_qty_in_wet, sac.total_qty_in_dry,
+                   sac.current_qty_dry, sac.current_qty_wet,
+                   sac.grd_qty_unit_id
+              FROM sac_stock_assay_content sac
+            -- WHERE sac.internal_gmr_ref_no = 'GMR-58'
+          GROUP BY sac.internal_gmr_ref_no,
+                   sac.internal_grd_ref_no,
+                   sac.total_qty_in_wet,
+                   sac.total_qty_in_dry,
+                   sac.current_qty_dry,
+                   sac.current_qty_wet,
+                   sac.grd_qty_unit_id) t
+GROUP BY t.internal_gmr_ref_no, t.grd_qty_unit_id
