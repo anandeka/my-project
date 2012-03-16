6929cc794d4cd4703149108c79eff03b1a7e229d
mohitkumar.gupta <mohitkumar.gupta@ekaplus.com>
2012-03-16 15:11:59 +0000

    For API,PFI,VAT invoices o/p doc.

---
+++ DBScripts/ObjectsScripts/object_script_228.sql
@@ -0,0 +1,5 @@
+alter table API_D add (TOTAL_TAX_AMOUNT varchar2(38),TOTAL_OTHER_CHARGE_AMOUNT varchar2(38)) ;
+
+alter table PFI_D add (TOTAL_TAX_AMOUNT varchar2(38),TOTAL_OTHER_CHARGE_AMOUNT varchar2(38)) ;
+
+alter table API_D add (PRICE varchar2(38),PRICE_UNIT varchar2(15)) ;
