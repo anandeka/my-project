bcd2646c25dbacd820fb712414dac129d331a97c
sivachalabathi.s <sivachalabathi.s@ekaplus.com>
2012-03-20 16:21:18 +0000

    metal_balance_dashboard

---
+++ DBScripts/MasterScripts/Master_Scripts_241.sql
@@ -0,0 +1,3 @@
+set define off;
+insert into amc_app_menu_configuration (MENU_ID, MENU_DISPLAY_NAME, DISPLAY_SEQ_NO, MENU_LEVEL_NO, LINK_CALLED, ICON_CLASS, MENU_PARENT_ID, ACL_ID, TAB_ID, FEATURE_ID, IS_DELETED)
+values ('BI-19', 'Metal Balance', 14, 2, '/metalBalanceDashboardForwardServlet', '', 'BI-1', 'APP-ACL-N897', 'Analytics', 'APP-PFL-N-161', 'N');
