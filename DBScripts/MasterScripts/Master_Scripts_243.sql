d218a1fab4a438c61ce0de63f1b7e69a223a5f55
suresh.gottipati <suresh.gottipati@ekaplus.com>
2012-03-21 07:25:03 +0000

    Bug Fixes

---
+++ DBScripts/MasterScripts/Master_Scripts_243.sql
@@ -0,0 +1,7 @@
+SET DEFINE OFF;
+INSERT INTO ref_reportexportformat
+            (report_id, export_format, report_file_name
+            )
+     VALUES ('242', 'EXCEL', 'YTDMTDYIELD_Excel.rpt'
+            );
+COMMIT ;
