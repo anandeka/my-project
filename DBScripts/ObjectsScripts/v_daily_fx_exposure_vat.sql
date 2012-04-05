9ff5285cc4f30bea43b6b0ce142da3ed701c160e
saurabh.raj <saurabh.raj@ekaplus.com>
2012-04-05 09:10:51 +0000

    Added section for free metals

--- DBScripts/ObjectsScripts/v_daily_fx_exposure_vat.sql
+++ DBScripts/ObjectsScripts/v_daily_fx_exposure_vat.sql
@@ -6,7 +6,7 @@ select akc.corporate_id,
        cm_base.cur_code base_currency,
        'Physicals' main_section,
        'Vat' section,
-       '' sub_section,
+       NULL sub_section,
        cpc.profit_center_id,
        cpc.profit_center_short_name profit_center,
        pdm.product_id,
@@ -96,18 +96,21 @@ select akc.corporate_id,
    and pcpd.product_id = pdm.product_id
    and akc.base_cur_id = cm_base.cur_id
    and ivd.vat_remit_cur_id = cm_pay.cur_id
-   and akc.base_cur_id = cm_base.cur_id   
+   and akc.base_cur_id = cm_base.cur_id
+   
    and nvl(ivd.vat_amount_in_vat_cur,0) <> 0
    and iis.is_active = 'Y'
    and iis1.is_active = 'Y'
    and gmr.is_deleted = 'N'
+   
+   
 union all 
  select akc.corporate_id,
        akc.corporate_name,
        cm_base.cur_code base_currency,
        'Physicals' main_section,
        'Vat' section,
-       '' sub_section,
+       NULL sub_section,
        cpc.profit_center_id,
        cpc.profit_center_short_name profit_center,
        pdm.product_id,
@@ -197,6 +200,7 @@ union all
    and akc.base_cur_id = cm_base.cur_id                                                                                
    and iis.is_active = 'Y'
    and gmr.is_deleted = 'N'
+  
 union all ---for sales contract when invoice cur and vat cur are not same   outflow 
   select akc.corporate_id,
        akc.corporate_name,
@@ -293,5 +297,153 @@ union all ---for sales contract when invoice cur and vat cur are not same   outf
    and pcm.purchase_sales = 'S'
    and akc.base_cur_id = cm_base.cur_id
    and iis.is_active = 'Y'
-   and gmr.is_deleted = 'N'
-/
+   and gmr.is_deleted = 'N'   
+union all --- Free Metal
+   select akc.corporate_id,
+       akc.corporate_name,
+       cm_base.cur_code base_currency,
+       'Physicals' main_section,
+       'Price Fixation' section,
+       'Spot Fixations' sub_section,
+       cpc.profit_center_id,
+       cpc.profit_center_short_name profit_center,
+       pdm_under.product_id,
+       pdm_under.product_desc product,
+       pcm.trader_id trader_id,
+       gab.firstname || ' ' || gab.lastname trader,
+       cm_pay.cur_id exposure_cur_id,
+       cm_pay.cur_code exposure_currency,
+       pfd.as_of_date trade_date, --pcm.issue_date trade_date,
+       pkg_general.f_get_converted_currency_amt(akc.corporate_id,
+                                                cm_base.cur_id,
+                                                cm_pay.cur_id,
+                                                pfd.as_of_date,
+                                                1) fx_rate,
+       pcm.contract_ref_no,
+        '' invoice_ref_no,
+       '' parent_invoice_no,
+       pcm.contract_ref_no || ' - ' || pcdi.delivery_item_no delivery_item_ref_no,
+       pcm.contract_ref_no || ' Item No. ' || pci.del_distribution_item_no contract_item_ref_no,
+       gmr.gmr_ref_no gmr_ref_no,
+       aml.attribute_name element_name,
+       null currency_pair,
+       pcdi.payment_due_date expected_payment_due_date,
+       pfqpp.qp_period_from_date qp_start_date,
+       pfqpp.qp_period_to_date qp_end_date,
+       (case
+         when pfqpp.qp_pricing_period_type = 'Month' then
+          pfqpp.qp_month || ' - ' || pfqpp.qp_year
+         when pfqpp.qp_pricing_period_type = 'Event' then
+          pfqpp.no_of_event_months || ' ' || pfqpp.event_name
+         when pfqpp.qp_pricing_period_type = 'Period' then
+          to_char(pfqpp.qp_period_from_date, 'dd-Mon-yyyy') || ' to ' ||
+          to_char(pfqpp.qp_period_to_date, 'dd-Mon-yyyy')
+       end) qp,
+       to_date(('01' || pci.expected_delivery_month || '-' ||
+               pci.expected_delivery_year),
+               'dd-Mon-yyyy') delivery_month,
+       pym.payment_term payment_terms,
+       pofh.per_day_pricing_qty qty,
+       qum.qty_unit,
+       qum.qty_unit_id,
+       qum.decimals qty_decimals,
+       pfd.user_price price,
+       pum.price_unit_id,
+       pum.price_unit_name price_unit,
+       null payable_receivable,
+       decode(pcm.purchase_sales, 'P', -1, 'S', 1) *
+       (pfd.user_price / nvl(ppu.weight, 1)) *
+       (round(pkg_general.f_get_converted_currency_amt(akc.corporate_id,
+                                                       ppu.cur_id,
+                                                       cm_pay.cur_id,
+                                                       pfd.as_of_date,
+                                                       1),
+              5) *
+        pkg_general.f_get_converted_quantity(nvl(pdm_under.product_id,
+                                                 pdm.product_id),
+                                             qum.qty_unit_id,
+                                             pum.weight_unit_id,
+                                             pofh.per_day_pricing_qty)) hedging_amount,
+       '' cost_type,
+       null effective_date,
+       '' buy_sell,
+       null value_date
+  from pci_physical_contract_item     pci,
+       pcdi_pc_delivery_item          pcdi,
+       pcm_physical_contract_main     pcm,
+       poch_price_opt_call_off_header poch,
+       aml_attribute_master_list      aml,
+       pdm_productmaster              pdm_under,
+       qum_quantity_unit_master       qum_under,
+       pocd_price_option_calloff_dtls pocd,
+       pofh_price_opt_fixation_header pofh,
+       pfd_price_fixation_details     pfd,
+       pcbpd_pc_base_price_detail     pcbpd,
+       ppfh_phy_price_formula_header  ppfh,
+       pfqpp_phy_formula_qp_pricing   pfqpp,
+       gmr_goods_movement_record      gmr,
+       -- pcpch_pc_payble_content_header pcpch,
+       ak_corporate                akc,
+       ak_corporate_user           akcu,
+       gab_globaladdressbook       gab,
+       pcpd_pc_product_definition  pcpd,
+       pym_payment_terms_master    pym,
+       cpc_corporate_profit_center cpc,
+       pdm_productmaster           pdm,
+       cm_currency_master          cm_base,
+       cm_currency_master          cm_pay,
+       v_ppu_pum                   ppu,
+       pum_price_unit_master       pum,
+       qum_quantity_unit_master    qum
+ where pcdi.pcdi_id = pci.pcdi_id
+   and pcdi.internal_contract_ref_no = pcm.internal_contract_ref_no
+   and pcdi.pcdi_id = poch.pcdi_id
+   and poch.is_free_metal_pricing = 'Y'
+   and poch.element_id = aml.attribute_id
+   and aml.underlying_product_id = pdm_under.product_id(+)
+   and pdm_under.base_quantity_unit = qum_under.qty_unit_id(+)
+   and poch.poch_id = pocd.poch_id
+   and pocd.pocd_id = pofh.pocd_id(+)
+   and pofh.pofh_id = pfd.pofh_id
+   and pocd.pcbpd_id = pcbpd.pcbpd_id
+   and pcbpd.pcbpd_id = ppfh.pcbpd_id(+)
+   and ppfh.ppfh_id = pfqpp.ppfh_id(+)
+   and nvl(pfqpp.is_qp_any_day_basis, 'N') <> 'Y'
+   and pofh.internal_gmr_ref_no = gmr.internal_gmr_ref_no(+)
+      /* and pcpch.internal_contract_ref_no = pcm.internal_contract_ref_no
+               and nvl(pcpch.payable_type, 'Payable') = 'Payable'
+               and poch.element_id = pcpch.element_id*/
+   and pcm.corporate_id = akc.corporate_id
+   and pcm.trader_id = akcu.user_id(+)
+   and akcu.gabid = gab.gabid
+   and pcm.internal_contract_ref_no = pcpd.internal_contract_ref_no(+)
+   and pcm.payment_term_id = pym.payment_term_id
+   and pcpd.profit_center_id = cpc.profit_center_id
+   and pcpd.product_id = pdm.product_id
+   and akc.base_cur_id = cm_base.cur_id
+   and pocd.pay_in_cur_id = cm_pay.cur_id
+   and pfd.price_unit_id = ppu.product_price_unit_id(+)
+   and ppu.price_unit_id = pum.price_unit_id(+)
+   and pocd.qty_to_be_fixed_unit_id = qum.qty_unit_id
+   and pcbpd.price_basis <> 'Fixed'
+   and pcm.contract_type = 'CONCENTRATES'
+      -- and pcm.approval_status = 'Approved'
+   and (case when pcm.is_tolling_contract = 'Y' then
+        nvl(pcm.approval_status, 'Approved') else pcm.approval_status end) =
+       'Approved'
+   and pcdi.is_active = 'Y'
+   and pci.is_active = 'Y'
+   and pcm.is_active = 'Y'
+   and nvl(gmr.is_deleted, 'N') = 'N'
+   and pcm.contract_status <> 'Cancelled'
+   and poch.is_active = 'Y'
+   and pocd.is_active = 'Y'
+   and pofh.is_active(+) = 'Y'
+   and pcbpd.is_active = 'Y'
+   and ppfh.is_active(+) = 'Y'
+   and pfqpp.is_active(+) = 'Y'
+   and pym.is_active = 'Y'
+   and pym.is_deleted = 'N'
+   and akc.corporate_id = '{?CorporateID}'
+   and to_char(pfd.as_of_date, 'dd-Mon-yyyy') = '{?AsOfDate}'
+/   
