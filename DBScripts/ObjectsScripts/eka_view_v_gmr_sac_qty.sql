19560da98b3f5d536695ec21b76626bc3466a459
ashokkumar.l <ashokkumar.l@ekaplus.com>
2012-04-17 10:11:13 +0000

    Bug Fixes

--- DBScripts/ObjectsScripts/eka_view_v_gmr_sac_qty.sql
+++ DBScripts/ObjectsScripts/eka_view_v_gmr_sac_qty.sql
@@ -1,20 +1,23 @@
-CREATE OR REPLACE VIEW v_gmr_sac_qty 
-AS
-SELECT   t.internal_gmr_ref_no, SUM (t.total_qty_in_wet) total_qty_in_wet,
-         SUM (t.total_qty_in_dry) total_qty_in_dry,
-         SUM (t.current_qty_dry) current_qty_dry,
-         SUM (t.current_qty_wet) current_qty_wet, t.grd_qty_unit_id
-    FROM (SELECT   sac.internal_gmr_ref_no, sac.internal_grd_ref_no,
+create or replace view v_gmr_sac_qty 
+as
+select   t.internal_gmr_ref_no, sum (t.total_qty_in_wet) total_qty_in_wet,
+         sum (t.total_qty_in_dry) total_qty_in_dry,
+         sum (t.current_qty_dry) current_qty_dry,
+         sum (t.current_qty_wet) current_qty_wet, t.grd_qty_unit_id
+    from (select   sac.internal_gmr_ref_no, sac.internal_grd_ref_no,
                    sac.total_qty_in_wet, sac.total_qty_in_dry,
                    sac.current_qty_dry, sac.current_qty_wet,
                    sac.grd_qty_unit_id
-              FROM sac_stock_assay_content sac
-            -- WHERE sac.internal_gmr_ref_no = 'GMR-58'
-          GROUP BY sac.internal_gmr_ref_no,
+              from sac_stock_assay_content sac,
+                   grd_goods_record_detail grd
+              where sac.internal_grd_ref_no=grd.internal_grd_ref_no
+                   and grd.status='Active'
+                   and grd.is_deleted='N'                      
+          group by sac.internal_gmr_ref_no,
                    sac.internal_grd_ref_no,
                    sac.total_qty_in_wet,
                    sac.total_qty_in_dry,
                    sac.current_qty_dry,
                    sac.current_qty_wet,
                    sac.grd_qty_unit_id) t
-GROUP BY t.internal_gmr_ref_no, t.grd_qty_unit_id
+group by t.internal_gmr_ref_no, t.grd_qty_unit_id
