db68ab7785e0ca98c95f7f682db9311f3f038f55
anandp <anand.hulbatte@ekaplus.com>
2012-04-19 14:39:54 +0000

    For commercial invoice document.

---
+++ DBScripts/MasterScripts/Master_Scripts_279.sql
@@ -0,0 +1,6 @@
+Insert into DM_DOCUMENT_MASTER
+   (DOC_ID, DOC_NAME, DISPLAY_ORDER, VERSION, IS_ACTIVE, 
+    IS_DELETED, ACTIVITY_ID)
+ Values
+   ('CREATE_CFI', 'Commercial Fee Invoice', 100, NULL, 'Y', 
+    'N', NULL);
