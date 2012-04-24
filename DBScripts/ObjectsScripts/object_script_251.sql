f1a7b7fc71aa770210ea7a8961ea923ceb0cc3f7
anandp <anand.hulbatte@ekaplus.com>
2012-04-24 09:39:19 +0000

    For adv payment adjustment amount.

---
+++ DBScripts/ObjectsScripts/object_script_251.sql
@@ -0,0 +1,7 @@
+create table API_DETAILS_D
+(INTERNAL_INVOICE_REF_NO    varchar2(30),
+API_INVOICE_REF_NO          varchar2(30),
+API_AMOUNT_ADJUSTED         varchar2(45),
+INVOICE_CURRENCY            varchar2(15),
+INTERNAL_DOC_REF_NO         varchar2(30)
+);
