ea7cf0dab4c326b14112ab657625507f7e9abb8a
sivachalabathi.s <sivachalabathi.s@ekaplus.com>
2012-04-25 10:00:03 +0000

    bug fixes, base qty unit column was wrongly placed

--- DBScripts/ObjectsScripts/EKA_MV_BI_HEDGED_UNHEDGED_POSITION.sql
+++ DBScripts/ObjectsScripts/EKA_MV_BI_HEDGED_UNHEDGED_POSITION.sql
@@ -1,7 +1,7 @@
 drop materialized view mv_bi_hedged_unhedged_position;
 /
 create materialized view mv_bi_hedged_unhedged_position
-build immediate 
+build immediate
 refresh force  
 on demand
 START WITH TO_DATE('01-Feb-2012 16:57:03','dd-mon-yyyy hh24:mi:ss')
