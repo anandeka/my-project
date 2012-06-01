6f76789b094899d57da071ea0c16ed3999a7ec54
sriveenam <sriveena.mattaparthi@ekaplus.com>
2012-06-01 13:03:39 +0000

    column size increased

---
+++ DBScripts/ObjectsScripts/object_script_263.sql
@@ -0,0 +1,4 @@
+alter table PCDD_DOCUMENT_DETAILS modify doc_type varchar2(100);
+alter table PCDDUL_DOCUMENT_DETAILS_UL modify doc_type varchar2(100);
+
+alter table PQPA_PQ_PHYSICAL_ATTRIBUTES modify ATTRIBUTE_VALUE varchar2(100);
