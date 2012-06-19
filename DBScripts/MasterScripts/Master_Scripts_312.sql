5ed3e6f75f8a879e14d056b2a2e65cc2ce21ba96
saurabh.raj <saurabh.raj@ekaplus.com>
2012-06-19 13:09:06 +0000

    Excel Format for Daily Balance position report

---
+++ DBScripts/MasterScripts/Master_Scripts_312.sql
@@ -0,0 +1,6 @@
+delete from REF_REPORTEXPORTFORMAT where report_id = '251';
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('251', 'EXCEL', 'DailyPositionBalanceReport_Excel.rpt');
+
