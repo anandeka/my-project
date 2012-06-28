80bd804480f0e1a90bccfe9a4ea599e57d7ddf84
sriveenam <sriveena.mattaparthi@ekaplus.com>
2012-06-28 11:22:10 +0000

    static tag scripts

---
+++ DBScripts/MasterScripts/Master_Scripts_317.sql
@@ -0,0 +1,10 @@
+
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('freemetalpricebasis', 'Index', 'N', 1);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('freemetalpricebasis', 'Formula', 'N', 2);
+
