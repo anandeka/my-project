00d905721dce7535706d203e25cd2bfe96d002b7
mohitkumar.gupta <mohitkumar.gupta@ekaplus.com>
2012-07-04 10:18:18 +0000

    For COG.

---
+++ DBScripts/MasterScripts/Master_Scripts_322.sql
@@ -0,0 +1,6 @@
+Insert into AKM_ACTION_REF_KEY_MASTER
+   (ACTION_KEY_ID, ACTION_KEY_DESC, VALIDATION_QUERY)
+ Values
+   ('CancelWNS', 'Cancel WNS Ref No', 'SELECT COUNT(*) FROM   AXS_ACTION_SUMMARY axs WHERE  axs.action_ref_no = :pc_action_ref_no AND    axs.corporate_id = :pc_corporate_id');
+
+
