5bde3b2c4b0c23f986420ecd7deadc0e8bf38095
jana <janardhana@ekaplus.com>
2012-07-09 07:07:02 +0000

    Modified to use the table

--- DBScripts/EODScripts/Eka_View_v_pcdi_exchange_detail.sql
+++ DBScripts/EODScripts/Eka_View_v_pcdi_exchange_detail.sql
@@ -1,23 +1,14 @@
-create or replace view v_pcdi_exchange_detail as 
-select t.process_id,
-       pci.pcdi_id,
-       t.element_id,
-       t.instrument_id,
-       t.instrument_name,
-       t.derivative_def_id,
-       t.derivative_def_name,
-       t.exchange_id,
-       t.exchange_name
-  from v_contract_exchange_detail t,
-       pci_physical_contract_item pci
- where pci.process_id = t.process_id
-   and pci.internal_contract_item_ref_no = t.internal_contract_item_ref_no
- group by t.process_id,
-          pci.pcdi_id,
-          t.element_id,
-          t.instrument_id,
-          t.instrument_name,
-          t.derivative_def_id,
-          t.derivative_def_name,
-          t.exchange_id,
-          t.exchange_name
+create or replace view v_pcdi_exchange_detail as
+   SELECT   t.corporate_id, t.pcdi_id, t.element_id, t.instrument_id,
+            t.instrument_name, t.derivative_def_id, t.derivative_def_name,
+            t.exchange_id, t.exchange_name
+       FROM ced_contract_exchange_detail t
+   GROUP BY t.corporate_id,
+            t.pcdi_id,
+            t.element_id,
+            t.instrument_id,
+            t.instrument_name,
+            t.derivative_def_id,
+            t.derivative_def_name,
+            t.exchange_id,
+            t.exchange_name;
