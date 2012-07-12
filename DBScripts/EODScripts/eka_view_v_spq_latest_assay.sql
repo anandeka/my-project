a14063ce1c6e256e662b255b589a7594b46cc72b
suresh.gottipati <suresh.gottipati@ekaplus.com>
2012-07-12 10:54:44 +0000

    Bug Fixes

---
+++ DBScripts/EODScripts/eka_view_v_spq_latest_assay.sql
@@ -0,0 +1,11 @@
+create or replace view v_spq_latest_assay as
+select spq.process_id,
+       spq.internal_gmr_ref_no,
+       spq.internal_grd_ref_no,
+       spq.assay_header_id
+  from spq_stock_payable_qty spq
+ where spq.is_stock_split = 'N'
+ group by spq.internal_gmr_ref_no,
+          spq.internal_grd_ref_no,
+          spq.assay_header_id,
+          spq.process_id
