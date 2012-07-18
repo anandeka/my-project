3bf61c183b486254bc1b04e3da34145effb1da19
mayank.chaturvedi <mayank.chaturvedi@ekaplus.com>
2012-07-18 12:00:58 +0000

    -Updating script for SCM_SERVICE_CHARGE_MASTER Table entries

---
+++ DBScripts/MasterScripts/Master_Scripts_331.sql
@@ -0,0 +1,5 @@
+set define off;
+update SCM_SERVICE_CHARGE_MASTER set COST_COMPONENT_NAME = 'Assay Charge' where COST_ID = 'SCM-24';
+update SCM_SERVICE_CHARGE_MASTER set COST_COMPONENT_NAME = 'Sampling Charge' where COST_ID = 'SCM-26';
+commit;
+
