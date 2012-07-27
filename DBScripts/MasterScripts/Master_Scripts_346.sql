a94e3685d1cd676d6d4d4c946ebefd3700fc0916
pradeep.kumar <pradeep.kumar@ekaplus.com>
2012-07-27 06:54:39 +0000

    -- SLV/SLS Entries for IIOPerations Activity in List Of Invoicable Items

---
+++ DBScripts/MasterScripts/Master_Scripts_346.sql
@@ -0,0 +1,95 @@
+Insert into SLV_STATIC_LIST_VALUE
+   (VALUE_ID, VALUE_TEXT)
+ Values
+   ('RECORD_OUT_PUT_TOLLING', 'Receive Material');
+Insert into SLV_STATIC_LIST_VALUE
+   (VALUE_ID, VALUE_TEXT)
+ Values
+   ('CREATE_FREE_MATERIAL', 'Capture Yield');
+Insert into SLV_STATIC_LIST_VALUE
+   (VALUE_ID, VALUE_TEXT)
+ Values
+   ('MARK_FOR_TOLLING', 'Mark For Tolling');
+
+
+
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'shipmentDetail', 'N', 1);
+   Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'railDetail', 'N', 2);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'truckDetail', 'N', 3);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'airDetail', 'N', 4);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'landingDetail', 'N', 5);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'warehouseReceipt', 'N', 6);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'weightNote', 'N', 7);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'shipmentAdvise', 'N', 8);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'truckAdvice', 'N', 9);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'airAdvice', 'N', 10);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'railAdvice', 'N', 11);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'salesLandingDetail', 'N', 12);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'releaseOrder', 'N', 13);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'salesWeightNote', 'N', 14);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'shipmentBackToBack', 'N', 15);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'RECORD_OUT_PUT_TOLLING', 'N', 16);
+   Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'CREATE_FREE_MATERIAL', 'N', 17);
+   Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'MARK_FOR_TOLLING', 'N', 18);
+   Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'financialSettlement', 'N', 19);
+   Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('IIOperationsActivity', 'pledgeTransfer', 'N', 20);
