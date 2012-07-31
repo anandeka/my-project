0333ac4dc8e39c027d9c9ae0e9cfb38ad5e2ba86
anupkumar.agarwal <anupkumar.agarwal@ekaplus.com>
2012-07-31 11:42:33 +0000

    Add New Service Key For List Of GMR Filter Search Criteria

---
+++ DBScripts/MasterScripts/Master_Scripts_353.sql
@@ -0,0 +1,50 @@
+
+INSERT INTO slv_static_list_value
+            (value_id, value_text
+            )
+     VALUES ('ContractItemRefNo', 'Contract Item Ref No.'
+            );
+
+INSERT INTO sls_static_list_setup
+            (list_type, value_id, is_default, display_order
+            )
+     VALUES ('GMRSearchCriteria', 'GMR Ref No', 'N', 1
+            );
+INSERT INTO sls_static_list_setup
+            (list_type, value_id, is_default, display_order
+            )
+     VALUES ('GMRSearchCriteria', 'Contract Ref No', 'N', 2
+            );
+
+INSERT INTO sls_static_list_setup
+            (list_type, value_id, is_default, display_order
+            )
+     VALUES ('GMRSearchCriteria', 'ContractItemRefNo', 'N', 3
+            );
+
+INSERT INTO sls_static_list_setup
+            (list_type, value_id, is_default, display_order
+            )
+     VALUES ('GMRSearchCriteria', 'Container No', 'N', 4
+            );
+INSERT INTO sls_static_list_setup
+            (list_type, value_id, is_default, display_order
+            )
+     VALUES ('GMRSearchCriteria', 'B/L No', 'N', 5
+            );
+INSERT INTO sls_static_list_setup
+            (list_type, value_id, is_default, display_order
+            )
+     VALUES ('GMRSearchCriteria', 'ActivityRefNo', 'N', 6
+            );
+INSERT INTO sls_static_list_setup
+            (list_type, value_id, is_default, display_order
+            )
+     VALUES ('GMRSearchCriteria', 'VoyageNo', 'N', 7
+            );
+
+INSERT INTO sls_static_list_setup
+            (list_type, value_id, is_default, display_order
+            )
+     VALUES ('GMRSearchCriteria', 'StockRefNo', 'N', 8
+            );
