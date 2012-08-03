17c55e7e46041b1fdc020cad679d5eb72c624046
pradeep.kumar <pradeep.kumar@ekaplus.com>
2012-08-03 09:52:54 +0000

    -- SLV/SLS Scripts for Yield Captured Dropdown Filter in List Of Tolling Input/OutPut GMR

---
+++ DBScripts/MasterScripts/Master_Scripts_360.sql
@@ -0,0 +1,20 @@
+Insert into SLV_STATIC_LIST_VALUE
+   (VALUE_ID, VALUE_TEXT)
+ Values
+   ('N/A', 'N/A');
+
+
+
+
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('YesNoNA', 'Y', 'N', 1);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('YesNoNA', 'N', 'N', 2);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('YesNoNA', 'N/A', 'N', 3);
