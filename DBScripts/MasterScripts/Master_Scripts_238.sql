00bec3a7cc037f69235aa9cd9817c31f04a2b71c
pradeep.kumar <pradeep.kumar@ekaplus.com>
2012-08-17 13:21:23 +0000

    -- Added New SLS Entry for Activity Type DropDown in Tolling Input OutPut DropdownAnd Removed The same from Previous Patch To Avoid collision

--- DBScripts/MasterScripts/Master_Scripts_238.sql
+++ DBScripts/MasterScripts/Master_Scripts_238.sql
@@ -9,12 +9,4 @@ Insert into SLV_STATIC_LIST_VALUE
 Insert into SLS_STATIC_LIST_SETUP
    (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
  Values
-   ('TollingInputOutputActivity', 'MARK_FOR_TOLLING', 'N', 1);
-Insert into SLS_STATIC_LIST_SETUP
-   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
- Values
-   ('TollingInputOutputActivity', 'RECORD_OUT_PUT_TOLLING', 'N', 2);
-Insert into SLS_STATIC_LIST_SETUP
-   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
- Values
    ('TollingInputOutputActivity', 'CREATE_RETURN_MATERIAL', 'N', 3);
---
+++ DBScripts/MasterScripts/Master_Scripts_374.sql
@@ -0,0 +1,9 @@
+
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('TollingInputOutputActivity', 'MARK_FOR_TOLLING', 'N', 1);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('TollingInputOutputActivity', 'RECORD_OUT_PUT_TOLLING', 'N', 2);
