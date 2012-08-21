fd7cb37a470b33aff1b7f1c71ec27a26a94c932d
mayank.chaturvedi <mayank.chaturvedi@ekaplus.com>
2012-08-21 14:20:04 +0000

    -For bug id 63299 list of invoice filter issue

---
+++ DBScripts/MasterScripts/Master_Scripts_377.sql
@@ -0,0 +1,2 @@
+update sls_static_list_setup set value_id='To Be Posted' where display_order='6' AND list_type = 'PostingStatus';
+update sls_static_list_setup set value_id='Posted' where display_order='7' AND list_type='PostingStatus';
