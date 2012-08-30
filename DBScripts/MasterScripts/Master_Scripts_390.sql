068c2fb64120188bdd3cab0a3507ba282f112a26
manjavva.bevoor <anand.hulbatte@ekaplus.com>
2012-08-30 14:49:22 +0000

    DB scripts for Audit Log(Contracts)

---
+++ DBScripts/MasterScripts/Master_Scripts_390.sql
@@ -0,0 +1,345 @@
+SET DEFINE OFF;
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (3004, 'MODIFY_PC', NULL, 'internalContractRefNo',
+             'internalContractRefNo', NULL
+            );
+
+
+
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3005, 'MODIFY_PC', NULL, 'contractRefNo', 'contractRefNo', NULL
+            );
+
+
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3006, 'MODIFY_PC', NULL, 'issueDate', 'issueDate', NULL
+            );
+
+
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3008, 'MODIFY_PC', NULL, 'prefix', 'prefix', NULL
+            );
+
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3009, 'MODIFY_PC', NULL, 'middleNo', 'middleNo', NULL
+            );
+
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3010, 'MODIFY_PC', NULL, 'suffix', 'suffix', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label,
+             query_ref_id
+            )
+     VALUES (3011, 'MODIFY_PC', NULL, 'cpContractRefNo', 'cpContractRefNo',
+             NULL
+            );
+
+
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label,
+             query_ref_id
+            )
+     VALUES (3012, 'MODIFY_PC', NULL, 'partnershipType', 'partnershipType',
+             NULL
+            );
+
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (3013, 'MODIFY_PC', NULL, 'isInterCompanyDeal',
+             'isInterCompanyDeal', NULL
+            );
+
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3014, 'MODIFY_PC', NULL, 'isDraft', 'isDraft', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label,
+             query_ref_id
+            )
+     VALUES (3015, 'MODIFY_PC', NULL, 'cancellationDate', 'cancellationDate',
+             NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (3016, 'MODIFY_PC', NULL, 'isOptionalityContract',
+             'isOptionalityContract', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3017, 'MODIFY_PC', NULL, 'reasonToCancel', 'reasonToCancel', NULL
+            );
+
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label,
+             query_ref_id
+            )
+     VALUES (3018, 'MODIFY_PC', NULL, 'productGroupType', 'productGroupType',
+             NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3019, 'MODIFY_PC', NULL, 'contractType', 'contractType', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3020, 'MODIFY_PC', NULL, 'purchaseSales', 'purchaseSales', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3021, 'MODIFY_PC', NULL, 'corporateId', 'corporateId', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3022, 'MODIFY_PC', NULL, 'subCorporateId', 'subCorporateId', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3023, 'MODIFY_PC', NULL, 'contractStatus', 'contractStatus', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label,
+             query_ref_id
+            )
+     VALUES (3024, 'MODIFY_PC', NULL, 'prodQualComments', 'prodQualComments',
+             NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (3025, 'MODIFY_PC', NULL, 'trtmtChargeComments',
+             'trtmtChargeComments', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3026, 'MODIFY_PC', NULL, 'umpireRuleId', 'umpireRuleId', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3027, 'MODIFY_PC', NULL, 'samplingRules', 'samplingRules', NULL
+            );
+
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3028, 'MODIFY_PC', NULL, 'approvalStatus', 'approvalStatus', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (3029, 'MODIFY_PC', NULL, 'provisionalPymtPctg',
+             'provisionalPymtPctg', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (3030, 'MODIFY_PC', NULL, 'provisionalPymtAt',
+             'provisionalPymtAt', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3031, 'MODIFY_PC', NULL, 'paymentText', 'paymentText', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3032, 'MODIFY_PC', NULL, 'insurance', 'insurance', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3033, 'MODIFY_PC', NULL, 'taxes', 'taxes', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label,
+             query_ref_id
+            )
+     VALUES (3034, 'MODIFY_PC', NULL, 'genSaleCondition', 'genSaleCondition',
+             NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3035, 'MODIFY_PC', NULL, 'otherTerms', 'otherTerms', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label,
+             query_ref_id
+            )
+     VALUES (3036, 'MODIFY_PC', NULL, 'internalComments', 'internalComments',
+             NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label,
+             query_ref_id
+            )
+     VALUES (3037, 'MODIFY_PC', NULL, 'weightAllowance', 'weightAllowance',
+             NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (3038, 'MODIFY_PC', NULL, 'weightAllowanceUnitId',
+             'weightAllowanceUnitId', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3039, 'MODIFY_PC', NULL, 'unitOfMeasure', 'unitOfMeasure', NULL
+            );
+
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (3040, 'MODIFY_PC', NULL, 'isTollingContract',
+             'isTollingContract', NULL
+            );
+
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (3041, 'MODIFY_PC', NULL, 'isLotLevelInvoice',
+             'isLotLevelInvoice', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (3042, 'MODIFY_PC', NULL, 'internalActionRefNo',
+             'internalActionRefNo', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (3043, 'MODIFY_PC', NULL, 'isCommercialFeeApplied',
+             'isCommercialFeeApplied', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (3044, 'MODIFY_PC', NULL, 'passThrough', 'passThrough', NULL
+            );
+
+SET DEFINE OFF;
+INSERT INTO axed_action_entity_details
+            (ID, action_id,
+             sub_entity,
+             field_name, label, query_ref_id
+            )
+     VALUES (927, 'MODIFY_PC',
+             'productDefinitions.corporateStrategy.strategyId',
+             'profitCenterId', 'Profit Center', 304
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity,
+             field_name, label, query_ref_id
+            )
+     VALUES (922, 'MODIFY_PC', 'PhysicalContractMain.ourPersonInCharge',
+             'userId', 'Our Person In Charge', 302
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (923, 'MODIFY_PC', 'PhysicalContractMain.cp', 'profileid',
+             'CP Name', 324
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (929, 'MODIFY_PC', NULL, 'delScheduleComments',
+             'Delivery Schedule Comments', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label,
+             query_ref_id
+            )
+     VALUES (931, 'MODIFY_PC', NULL, 'internalComments', 'Internal Comments',
+             NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity,
+             field_name, label, query_ref_id
+            )
+     VALUES (924, 'MODIFY_PC', 'PhysicalContractMain.cpPersonInCharge',
+             'userId', 'CP Person In Charge', 325
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name, label, query_ref_id
+            )
+     VALUES (932, 'MODIFY_PC', NULL, 'otherTerms', 'Others', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (933, 'MODIFY_PC', 'ProductDefinition', 'productDefinitions',
+             'Product Quantity', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (928, 'MODIFY_PC', NULL, 'delBasisComments',
+             'Delivery Basis Comments', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (930, 'MODIFY_PC', NULL, 'basePriceComments',
+             'Base Price Comments', NULL
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id, sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (925, 'MODIFY_PC', 'PhysicalContractMain.trader', 'userId',
+             'Trader Name', 302
+            );
+INSERT INTO axed_action_entity_details
+            (ID, action_id,
+             sub_entity, field_name,
+             label, query_ref_id
+            )
+     VALUES (926, 'MODIFY_PC',
+             'productDefinitions.corporateStrategy.strategyId', 'strategyid',
+             'Strategy', 301
+            );
+COMMIT ;
