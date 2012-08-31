134228ac97894c343d0279ccb3a6a3d9749fc965
pradeep.kumar <pradeep.kumar@ekaplus.com>
2012-08-31 06:52:43 +0000

    -- Corrected...

--- DBScripts/MasterScripts/Master_Scripts_390.sql
+++ DBScripts/MasterScripts/Master_Scripts_390.sql
@@ -343,13 +343,3 @@ INSERT INTO axed_action_entity_details
              'Strategy', 301
             );
 COMMIT ;
-SET DEFINE OFF;
-Insert into QR_QUERY_REFERENCE
-   (QUERY_ID, QUERY_STRING)
- Values
-   (324, 'select COMPANYNAME from PHD_PROFILEHEADERDETAILS where PROFILEID=?');
-Insert into QR_QUERY_REFERENCE
-   (QUERY_ID, QUERY_STRING)
- Values
-   (325, 'SELECT gab.firstname||'' ''||gab.lastname FROM PHD_PROFILEHEADERDETAILS phd,gab_globaladdressbook gab WHERE phd.PROFILEID = gab.PROFILEID AND phd.PROFILEID = ?');
-COMMIT;
