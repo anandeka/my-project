ae7a019dc6194d9af4a40f40073d01a679d5206b
saurabh.raj <saurabh.raj@ekaplus.com>
2012-09-14 14:50:18 +0000

    Change Parameter Name in Delivery schedule report

---
+++ DBScripts/CorporateScripts/CorporateScripts_90.sql
@@ -0,0 +1,2 @@
+update RFC_REPORT_FILTER_CONFIG set LABEL = 'Delivery Period From' where LABEL_ID = 'RFC362PHY03';
+update RFC_REPORT_FILTER_CONFIG set LABEL = 'Deliver Period To' where LABEL_ID = 'RFC362PHY04';
