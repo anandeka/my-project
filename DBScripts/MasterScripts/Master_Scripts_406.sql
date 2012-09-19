afb8580437d6fba56ad8caa1c7e51d0992fe5712
pradeep.kumar <pradeep.kumar@ekaplus.com>
2012-09-19 11:50:44 +0000

    -- SLS/SLV Entries for Element Type (Payable/Returnable/None)

---
+++ DBScripts/MasterScripts/Master_Scripts_406.sql
@@ -0,0 +1,12 @@
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('ElementTyope', 'Payable', 'N', 1);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('ElementType', 'Returnable', 'N', 2);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('ElementType', 'None', 'N', 3);
