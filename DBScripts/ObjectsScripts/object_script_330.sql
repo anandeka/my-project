eabd7edce442adc2b9b496823b8ade8f7b47b054
pradeep.kumar <pradeep.kumar@ekaplus.com>
2012-09-21 09:55:17 +0000

    -- Added some extra columns in SADC_CHILD_DGRD_D for ReleaseOrder Document

---
+++ DBScripts/ObjectsScripts/object_script_330.sql
@@ -0,0 +1,10 @@
+---------------------------------------------------------------------------------
+---ADDED FOR RELEASE OREDER DOCUMENT
+---------------------------------------------------------------------------------
+
+ALTER TABLE SADC_CHILD_DGRD_D  ADD (DUTY_STATUS VARCHAR2 (100));
+
+ALTER TABLE SADC_CHILD_DGRD_D  ADD (CUSTOM_STATUS VARCHAR2 (100));
+
+ALTER TABLE SADC_CHILD_DGRD_D  ADD (TAX_STATUS VARCHAR2 (100));
+
