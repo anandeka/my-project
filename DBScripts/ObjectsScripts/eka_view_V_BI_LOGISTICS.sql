f3428e26081a57c2b057ec2c33d7ba2d77e2d748
suresh.gottipati <suresh.gottipati@ekaplus.com>
2012-10-04 12:30:42 +0000

    Bug Fixes

--- DBScripts/ObjectsScripts/eka_view_V_BI_LOGISTICS.sql
+++ DBScripts/ObjectsScripts/eka_view_V_BI_LOGISTICS.sql
@@ -1,34 +1,42 @@
 create or replace view v_bi_logistics as
-with v_ash as(select ash.ash_id,
-       sum(asm.net_weight) wet_weight,
-       sum(asm.dry_weight) dry_weight,
-       round((sum(asm.dry_weight) / sum(asm.net_weight)) * 100, 10) dry_wet_qty_ratio
-  from ash_assay_header         ash,
-       asm_assay_sublot_mapping asm
- where ash.ash_id = asm.ash_id
+with v_ash as(select gmr.internal_gmr_ref_no,
+       grd.internal_grd_ref_no,
+       gmr.wns_status,
+       ash.ash_id,
+       ash.assay_type,
+       sum(asm.net_weight) actual_wet_weight,
+       sum(asm.dry_weight) actual_dry_weight
+  from gmr_goods_movement_record gmr,
+       grd_goods_record_detail   grd,
+       ash_assay_header          ash,
+       asm_assay_sublot_mapping  asm
+ where gmr.internal_gmr_ref_no = grd.internal_gmr_ref_no
+   and gmr.internal_gmr_ref_no = ash.internal_gmr_ref_no
+   and grd.internal_grd_ref_no = ash.internal_grd_ref_no
+   and ash.assay_type = 'Weighing and Sampling Assay'
+   and ash.ash_id = asm.ash_id
+   and asm.is_active = 'Y'
+   and ash.is_active = 'Y'
+   and gmr.is_deleted = 'N'
+   and grd.status = 'Active'
+ group by gmr.internal_gmr_ref_no,
+          grd.internal_grd_ref_no,
+          ash.ash_id,
+          ash.assay_type,
+          gmr.wns_status), 
+ v_agrd_qty as(select gmr.internal_gmr_ref_no,
+       sum(asm.net_weight) bl_wet_weight,
+       sum(asm.dry_weight) bl_dry_weight
+  from gmr_goods_movement_record gmr,
+       ash_assay_header          ash,
+       asm_assay_sublot_mapping  asm
+ where gmr.internal_gmr_ref_no = ash.internal_gmr_ref_no
+   and ash.assay_type = 'Shipment Assay'
+   and ash.ash_id = asm.ash_id
    and ash.is_active = 'Y'
    and asm.is_active = 'Y'
- group by ash.ash_id), 
- v_agrd_qty as(
-select agrd.qty                 qty,
-       agrd.internal_grd_ref_no internal_grd_ref_no
-  from ash_assay_header ash,
-       agrd_action_grd  agrd
- where agrd.internal_grd_ref_no = ash.internal_grd_ref_no
-   and agrd.action_no = 1
-   and ash.assay_type = 'Shipment Assay'
-   and ash.is_active = 'Y'
-   and agrd.status = 'Active'),
-v_agrd_gmr_qty as(select agrd.internal_gmr_ref_no,
-       sum(agrd.qty) qty
-  from ash_assay_header ash,
-       agrd_action_grd  agrd
- where agrd.internal_grd_ref_no = ash.internal_grd_ref_no
-   and agrd.action_no = 1
-   and ash.assay_type = 'Shipment Assay'
-   and ash.is_active = 'Y'
-   and agrd.status = 'Active'
- group by agrd.internal_gmr_ref_no)
+   and gmr.is_deleted='N'
+ group by gmr.internal_gmr_ref_no)
 select t.groupid,
        t.corporate_group,
        t.corporate_id,
@@ -76,58 +84,20 @@ select t.groupid,
        t.warehouse_state_name,
        t.warehouse_city_id,
        t.warehouse_city_name,
-       t.assay_status,
-       t.bl_product_base_uom,
-       --Note: t.bl_wet_weight is null added to handle the stock re-arrangement
-       (case
-         when t.bl_wet_weight is null then
-          t.gmr_bl_qty
-         else
-          t.bl_wet_weight
-       end) bl_wet_weight,
-       (case
-         when t.bl_wet_weight is null then
-          t.gmr_bl_qty * (t.actual_dry_weight / t.actual_wet_weight)
-         else
-          t.bl_dry_weight
-       end) bl_dry_weight,
+       --t.assay_status,
+       t.bl_product_base_uom,      
+       t.bl_wet_weight,
+       t.bl_dry_weight,
        t.actual_product_base_uom,
        t.actual_wet_weight,
        t.actual_dry_weight,
-       -- (t.actual_dry_weight/t.actual_wet_weight) DRY_WET_QTY_RATIO,
-       --  t.gmr_bl_qty,
-       (case
-         when t.bl_wet_weight is null then
-          t.actual_wet_weight - t.gmr_bl_qty
-         else
-          t.actual_wet_weight - t.bl_wet_weight
-       end) wet_qty_diff,
-       --  (t.actual_wet_weight-t.bl_wet_weight  ) wet_qty_diff,
-      (case
-         when t.bl_wet_weight is null then --added to handle the stock re-arrangement
-         t.actual_dry_weight - ( t.gmr_bl_qty * (t.actual_dry_weight / t.actual_wet_weight))
-         else
-        t.actual_dry_weight-  t.bl_dry_weight
-       end)dry_qty_diff,
-       
-       round((case
-         when t.bl_wet_weight is null then
-          (t.actual_wet_weight - t.gmr_bl_qty) / t.gmr_bl_qty * 100 
-         else
-          (t.actual_wet_weight - t.bl_wet_weight) / t.bl_wet_weight * 100
-       end),10)wet_ratio,
-       round((case
-         when t.bl_wet_weight is null then --added to handle the stock re-arrangement
-         (t.actual_dry_weight -( t.gmr_bl_qty * (t.actual_dry_weight / t.actual_wet_weight))) / ( t.gmr_bl_qty * (t.actual_dry_weight / t.actual_wet_weight)) * 100
-         else
-        (t.actual_dry_weight - t.bl_dry_weight) / t.bl_dry_weight * 100
-       end),10)dry_ratio
-       --(t.actual_wet_weight - t.bl_wet_weight) / t.bl_wet_weight * 100 wet_ratio,
-      -- (t.actual_dry_weight - t.bl_dry_weight) / t.bl_dry_weight * 100 dry_ratio
-  from (select gcd.groupid,
-               --ash.ash_id,
+       (t.actual_wet_weight- t.bl_wet_weight) wet_qty_diff,
+       (t.actual_dry_weight-t.bl_dry_weight) dry_qty_diff,
+       ((t.actual_wet_weight- t.bl_wet_weight)/ t.bl_wet_weight)*100 wet_ratio,
+       ((t.actual_dry_weight- t.bl_dry_weight)/ t.actual_wet_weight)*100 dry_ratio   
+  from (select gcd.groupid,               
                gcd.groupname corporate_group,
-               gmr.corporate_id, --asm.DRY_WET_QTY_RATIO,
+               gmr.corporate_id, 
                akc.corporate_name,
                pcpd.profit_center_id,
                cpc.profit_center_name,
@@ -180,24 +150,21 @@ select t.groupid,
                sm_sld.state_name warehouse_state_name,
                sld.city_id warehouse_city_id,
                cim_sld.city_name warehouse_city_name,
-               ash.assay_type assay_status,
+               --ash.assay_type assay_status,
                qum.qty_unit bl_product_base_uom,
-               sum(agrd.qty) bl_wet_weight,
-               sum((agrd.qty * nvl(asm.dry_wet_qty_ratio, 100)) / 100) bl_dry_weight,
-               qum.qty_unit actual_product_base_uom,
-               (case
-                 when ash.assay_type = 'Weighing and Sampling Assay' then
-                  sum(asm.wet_weight)
-                 else
-                  sum(grd.qty)
-               end) actual_wet_weight,
-               (case
-                 when ash.assay_type = 'Weighing and Sampling Assay' then
-                  sum(asm.dry_weight)
-                 else
-                  sum((grd.qty * nvl(asm.dry_wet_qty_ratio, 100)) / 100)
-               end) actual_dry_weight,
-               agrd_gmr.qty gmr_bl_qty
+               agrd.bl_wet_weight bl_wet_weight,
+               agrd.bl_dry_weight bl_dry_weight,
+               qum.qty_unit actual_product_base_uom,               
+               (case when asm.wns_status='Completed' then
+                sum(asm.actual_wet_weight)
+                else
+                null
+                end) actual_wet_weight,
+                 (case when asm.wns_status='Completed' then
+                sum(asm.actual_dry_weight)
+                else
+                null
+                end) actual_dry_weight
           from gmr_goods_movement_record gmr,
                ak_corporate akc,
                grd_goods_record_detail grd,
@@ -241,12 +208,9 @@ select t.groupid,
                cym_countrymaster cym_load,
                sm_state_master sm_discharge,
                cim_citymaster cim_discharge,
-               cym_countrymaster cym_discharge,
-               ash_assay_header ash,
-               v_ash asm,
-               sam_stock_assay_mapping sam,
-               axs_action_summary axs,
-               v_agrd_gmr_qty agrd_gmr,
+               cym_countrymaster cym_discharge,         
+               v_ash asm,            
+               axs_action_summary axs,           
                v_agrd_qty agrd
          where gmr.corporate_id = akc.corporate_id
            and gmr.internal_gmr_ref_no = grd.internal_gmr_ref_no
@@ -279,11 +243,9 @@ select t.groupid,
            and gmr.loading_country_id = cym_load.country_id(+)
            and gmr.discharge_state_id = sm_discharge.state_id(+)
            and gmr.discharge_city_id = cim_discharge.city_id(+)
-           and gmr.discharge_country_id = cym_discharge.country_id(+)
-           and grd.internal_grd_ref_no = sam.internal_grd_ref_no
-           and sam.ash_id = ash.ash_id
-           and ash.ash_id = asm.ash_id
-           and nvl(ash.is_active, 'Y') = 'Y'
+           and gmr.discharge_country_id = cym_discharge.country_id(+)  
+           and grd.internal_gmr_ref_no=asm.internal_gmr_ref_no(+)
+           and grd.internal_grd_ref_no=asm.internal_grd_ref_no(+)
            and grd.is_afloat = 'N'
            and gmr.is_deleted = 'N'
            and gmr.is_internal_movement = 'N'
@@ -294,13 +256,10 @@ select t.groupid,
            and phd.is_active = 'Y'
            and qum.is_active = 'Y'
            and qat.is_active = 'Y'
-           and gcd.is_active = 'Y'
-           and sam.is_latest_pricing_assay = 'Y'
+           and gcd.is_active = 'Y'   
            and pcpd.input_output = 'Input'
            and grd.status = 'Active'
-           and grd.internal_grd_ref_no = agrd.internal_grd_ref_no(+) -- to handle the stock re-arrangement case, use the agrd_gmr table gmr qty
-           and gmr.internal_gmr_ref_no = agrd_gmr.internal_gmr_ref_no
-          -- and gmr.internal_gmr_ref_no = 'GMR-1559'
+           and gmr.internal_gmr_ref_no=agrd.internal_gmr_ref_no 
            and grd.tolling_stock_type = 'None Tolling'
          group by gcd.groupid,
                   gcd.groupname,
@@ -346,10 +305,11 @@ select t.groupid,
                   cym_sld.country_name,
                   sld.state_id,
                   sm_sld.state_name,
-                  sld.city_id,
-                  agrd_gmr.qty,
-                  cim_sld.city_name,
-                  ash.assay_type,
+                  agrd.bl_wet_weight,
+                  agrd.bl_dry_weight,
+                  sld.city_id,              
+                  cim_sld.city_name, 
+                  asm.wns_status,
                   qum.qty_unit,
                   pcpq.unit_of_measure,
                   qum.qty_unit) t
