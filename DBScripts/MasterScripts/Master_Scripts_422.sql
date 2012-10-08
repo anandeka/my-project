3327b9bc42c30f14ade901bde0a5fd493e2bb502
b.ravikumar <b.ravikumar@ekaplus.com>
2012-10-08 14:20:20 +0000

    filters added in LOI.

---
+++ DBScripts/MasterScripts/Master_Scripts_422.sql
@@ -0,0 +1,9 @@
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('invoiceStatus', 'Pending', 'N', 1);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('invoiceStatus', 'Cancelled', 'N', 2);
+
