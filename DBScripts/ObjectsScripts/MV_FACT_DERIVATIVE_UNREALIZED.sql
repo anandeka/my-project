75923b187b34d8f59817b21e06cea63ca34fbee3
sivachalabathi.s <sivachalabathi.s@ekaplus.com>
2012-10-10 14:12:48 +0000

    added new columns as per the tata coras requirement

--- DBScripts/ObjectsScripts/MV_FACT_DERIVATIVE_UNREALIZED.sql
+++ DBScripts/ObjectsScripts/MV_FACT_DERIVATIVE_UNREALIZED.sql
@@ -1,14 +1,7 @@
-DROP MATERIALIZED VIEW MV_FACT_DERIVATIVE_UNREALIZED;
-DROP TABLE MV_FACT_DERIVATIVE_UNREALIZED;
-CREATE MATERIALIZED VIEW MV_FACT_DERIVATIVE_UNREALIZED 
-NOCACHE
-LOGGING
-NOCOMPRESS
-NOPARALLEL
-BUILD IMMEDIATE
-REFRESH FORCE ON DEMAND
-WITH PRIMARY KEY
-AS 
+drop materialized view MV_FACT_DERIVATIVE_UNREALIZED;
+create materialized view MV_FACT_DERIVATIVE_UNREALIZED
+refresh force on demand
+as
 select dpd.corporate_id,
        dpd.corporate_name,
        pdm.product_type_id product_type,
@@ -21,6 +14,7 @@ select dpd.corporate_id,
        dpd.quality_id,
        dpd.quality_name,
        dpd.eod_trade_date eod_date,
+       tdc.process eod_eom_flag,
        'Derivatives' position_type,
        dpd.instrument_type || ' ' || (case
          when dpd.trade_type = 'Buy' then
@@ -31,8 +25,8 @@ select dpd.corporate_id,
        dpd.derivative_ref_no contract_ref_no,
        dpd.external_ref_no,
        dpd.trade_date issue_trade_date,
-       dpd.clearer_profile_id cp_id,
-       dpd.clearer_name cp_name,
+       nvl(dpd.clearer_profile_id, dpd.cp_profile_id) cp_id,
+       nvl(dpd.clearer_name, dpd.cp_name) cp_name,
        dpd.payment_term,
        dpd.purpose_name derivative_purpose,
        (case
@@ -75,15 +69,15 @@ select dpd.corporate_id,
        dpd.instrument_name valuation_instrument,
        dpd.derivative_def_id,
        dpd.derivative_def_name,
-       to_char(dpd.period_date, 'Mon-yyyy') valuation_month,
-       dpd.period_date value_date,
+       to_char(nvl(dpd.period_date, dpd.prompt_date), 'Mon-yyyy') valuation_month,
+       nvl(dpd.period_date, dpd.prompt_date) value_date,
        dpd.settlement_price m2m_settlement_price,
        dpd.settlement_price net_settlement_price,
        dpd.sett_price_cur_code || '/' || dpd.sett_price_weight ||
        dpd.sett_price_weight_unit settlement_price_unit,
-       dpd.market_value_in_trade_cur market_value_in_val_ccy,
-       dpd.base_cur_id market_value_cur_id,
-       dpd.base_cur_code market_value_cur_code,
+       dpd.market_value_in_trade_cur market_value_in_val_ccy, -- used trade cur code, as mv convetred into trade currency and stored
+       dpd.trade_main_cur_id market_value_cur_id,
+       dpd.trade_main_cur_code market_value_cur_code,
        dpd.prev_day_unr_pnl_in_base_cur,
        dpd.pnl_in_base_cur unrealized_pnl_in_base_cur,
        dpd.trade_day_pnl_in_base_cur pnl_change_in_base_currency,
@@ -99,13 +93,35 @@ select dpd.corporate_id,
        dpd.expiry_date,
        dpd.prompt_date,
        dpd.dr_id_name prompt_details,
-       to_char(dpd.prompt_date, 'Mon-YYYY') prompt_month_year,
-       to_char(dpd.prompt_date, 'YYYY') prompt_year,
+       to_char((case
+                 when dpd.period_date is null then
+                  (case
+                 when dpd.period_month is not null and dpd.period_year is not null then
+                  to_date('01-' || dpd.period_month || '-' || dpd.period_year,
+                          'dd-Mon-yyyy')
+                 else
+                  dpd.prompt_date
+               end) else dpd.period_date end), 'Mon-YYYY') prompt_month_year,
+       to_char((case
+                 when dpd.period_date is null then
+                  (case
+                 when dpd.period_month is not null and dpd.period_year is not null then
+                  to_date('01-' || dpd.period_month || '-' || dpd.period_year,
+                          'dd-Mon-yyyy')
+                 else
+                  dpd.prompt_date
+               end) else dpd.period_date end), 'YYYY') prompt_year,
        msa.attribute_value_1,
        msa.attribute_value_2,
        msa.attribute_value_3,
        msa.attribute_value_4,
-       msa.attribute_value_5
+       msa.attribute_value_5,
+       dpd.fixed_avg_price,
+       dpd.unfixed_avg_price,
+       dpd.clearer_comm_in_base,
+       dpd.clearer_exch_rate clearer_cur_to_base,
+       dpd.trade_value_in_base,
+       dpd.market_value_in_base
   from dpd_derivative_pnl_daily@eka_eoddb dpd,
        tdc_trade_date_closure@eka_eoddb   tdc,
        pdm_productmaster                  pdm,
@@ -119,7 +135,7 @@ select dpd.corporate_id,
    and dpd.corporate_id = akc.corporate_id
    and akc.is_active = 'Y'
    and cm_corp.cur_id = akc.base_cur_id
-   and tdc.process = 'EOD'
+  -- and tdc.process = 'EOD'
    and dpd.strategy_id = msa.startegy_id(+)
 union all
 select cpd.corporate_id,
@@ -134,6 +150,7 @@ select cpd.corporate_id,
        'NA' quality_id,
        'NA' quality_name,
        cpd.eod_trade_date eod_date,
+       tdc.process eod_eom_flag,
        'Fx' position_type,
        'Fx  ' || cpd.home_cur_buy_sell position_sub_type,
        cpd.ct_ref_no contract_ref_no,
@@ -198,7 +215,13 @@ select cpd.corporate_id,
        msa.attribute_value_2,
        msa.attribute_value_3,
        msa.attribute_value_4,
-       msa.attribute_value_5
+       msa.attribute_value_5,
+       0 fixed_avg_price,
+       0 unfixed_avg_price,
+       0 clearer_comm_in_base,
+       0 clearer_cur_to_base,
+       0 trade_value_in_base,
+       0 market_value_in_base
   from cpd_currency_pnl_daily@eka_eoddb        cpd,
        tdc_trade_date_closure@eka_eoddb        tdc,
        ct_currency_trade@eka_eoddb             ct,
@@ -217,7 +240,7 @@ select cpd.corporate_id,
    and ct.process_id = cpd.process_id
    and pym.payment_term_id(+) = ct.payment_terms_id
    and dpm.purpose_id = ct.purpose_id
-   and tdc.process = 'EOD'
+  -- and tdc.process = 'EOD'
    and cpd.corporate_id = ak.corporate_id
    and ak.corporate_id = tdc.corporate_id
    and gcd_group_id.groupid = ak.groupid
@@ -225,4 +248,3 @@ select cpd.corporate_id,
    and ak.base_cur_id = cm_corp.cur_id
    and cpd.product_name = pdm.product_desc
    and cpd.strategy_id = msa.startegy_id(+)
-/
