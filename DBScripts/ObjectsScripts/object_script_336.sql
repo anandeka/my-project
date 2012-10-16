613981e04bd0e802dbf2e324f32666a79ca0327e
abhishek.vijay <abhishek.vijay@ekaplus.com>
2012-10-16 05:53:40 +0000

    assay document layout change

---
+++ DBScripts/ObjectsScripts/object_script_336.sql
@@ -0,0 +1,5 @@
+alter table AS_ASSAY_D add BL_NO VARCHAR2 (50 Char);
+alter table AS_ASSAY_D add BL_DATE DATE;
+alter table AS_ASSAY_D add VESSEL_NAME VARCHAR2 (100 Char);
+alter table AS_ASSAY_D add MODE_OF_TRANSPORT VARCHAR2 (15 Char);
+alter table AS_ASSAY_D add CONTAINER_NO VARCHAR2 (500 Char);
