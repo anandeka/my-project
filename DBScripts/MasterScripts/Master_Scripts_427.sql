cb28da5ad793967985ba7676a8f34fc8a8a232b6
kotipalli.prakash <anand.hulbatte@ekaplus.com>
2012-10-30 12:09:26 +0000

    Bug_id : 69338 . filters issues fixed.

---
+++ DBScripts/MasterScripts/Master_Scripts_427.sql
@@ -0,0 +1,2 @@
+Insert into SLS_STATIC_LIST_SETUP (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER) Values ('CalledOffType', 'Physical', 'N', 1);
+Insert into SLS_STATIC_LIST_SETUP (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER) Values ('CalledOffType', 'Pricing', 'N', 2);
