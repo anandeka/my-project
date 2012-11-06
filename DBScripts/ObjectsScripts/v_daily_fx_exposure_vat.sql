791c870971db62c996bf39529046472de35005fc
elaiyaraja.nallathambi <elaiyaraja.nallathambi@ekaplus.com>
2012-11-06 12:11:33 +0000

    Bug ID:- 69803

--- DBScripts/ObjectsScripts/v_daily_fx_exposure_vat.sql
+++ DBScripts/ObjectsScripts/v_daily_fx_exposure_vat.sql
@@ -42,7 +42,7 @@ select akc.corporate_id,
        null price_unit,
        iis.payable_receivable payable_receivable,
        (decode(iis.payable_receivable, 'Payable', 1, 'Receivable', -1) *
-       ivd.vat_amount_in_vat_cur) hedging_amount,
+       abs(ivd.vat_amount_in_vat_cur)) hedging_amount,
        '' cost_type,
        null effective_date,
        '' buy_sell,
@@ -50,7 +50,10 @@ select akc.corporate_id,
        iis.invoice_created_date correction_date,
        null activity_type,
        null activity_date,
-       null cpname
+       null cpname,
+       cm_vat.cur_code vat_cur_code,
+       cm_invoice.cur_code invoice_cur_code
+   
   from ivd_invoice_vat_details ivd,
        (select iid.internal_contract_item_ref_no,
                iid.internal_contract_ref_no,
@@ -79,7 +82,9 @@ select akc.corporate_id,
        cpc_corporate_profit_center cpc,
        pdm_productmaster pdm,
        cm_currency_master cm_base,
-       cm_currency_master cm_pay
+       cm_currency_master cm_pay,
+       cm_currency_master cm_vat,
+       cm_currency_master cm_invoice
  where ivd.internal_invoice_ref_no = iid.internal_invoice_ref_no
    and iid.internal_invoice_ref_no = iis.internal_invoice_ref_no
    and iid.internal_gmr_ref_no = gmr.internal_gmr_ref_no
@@ -97,6 +102,8 @@ select akc.corporate_id,
    and akc.base_cur_id = cm_base.cur_id
    and ivd.vat_remit_cur_id = cm_pay.cur_id
    and akc.base_cur_id = cm_base.cur_id
+   and ivd.vat_remit_cur_id=cm_vat.cur_id
+   and ivd.invoice_cur_id=cm_invoice.cur_id
    and iis.is_active = 'Y'
    and gmr.is_deleted = 'N'
    and nvl(ivd.vat_amount_in_vat_cur,0)<>0
@@ -120,7 +127,7 @@ select akc.corporate_id,
        cm_pay.cur_id exposure_cur_id,
        cm_pay.cur_code exposure_currency,
        iis.invoice_issue_date trade_date,
-       1 fx_rate, -- no fx rate to show the vat amt in invoice currency - -siva       
+       ivd.fx_rate_vc_ic fx_rate,    
        pcm.contract_ref_no,
        iis.invoice_ref_no,
        iis.vat_parent_ref_no parent_invoice_no,
@@ -144,7 +151,7 @@ select akc.corporate_id,
        null price_unit,
        iis.payable_receivable payable_receivable,
        (decode(iis.payable_receivable, 'Payable', -1, 'Receivable', 1) *
-       ivd.vat_amount_in_inv_cur) hedging_amount,
+       abs(ivd.vat_amount_in_inv_cur)) hedging_amount,
        '' cost_type,
        null effective_date,
        '' buy_sell,
@@ -152,7 +159,9 @@ select akc.corporate_id,
        iis.invoice_created_date correction_date,
        null activity_type,
        null activity_date,
-       null cpname
+       null cpname,
+       cm_vat.cur_code vat_cur_code,
+       cm_invoice.cur_code invoice_cur_code
   from ivd_invoice_vat_details ivd,
        (select iid.internal_contract_item_ref_no,
                iid.internal_contract_ref_no,
@@ -181,7 +190,9 @@ select akc.corporate_id,
        cpc_corporate_profit_center cpc,
        pdm_productmaster pdm,
        cm_currency_master cm_base,
-       cm_currency_master cm_pay
+       cm_currency_master cm_pay,
+        cm_currency_master cm_vat,
+       cm_currency_master cm_invoice
  where ivd.internal_invoice_ref_no = iid.internal_invoice_ref_no
    and iid.internal_invoice_ref_no = iis.internal_invoice_ref_no
    and iid.internal_gmr_ref_no = gmr.internal_gmr_ref_no
@@ -199,6 +210,8 @@ select akc.corporate_id,
    and akc.base_cur_id = cm_base.cur_id
    and ivd.invoice_cur_id = cm_pay.cur_id
    and akc.base_cur_id = cm_base.cur_id
+   and ivd.vat_remit_cur_id=cm_vat.cur_id
+   and ivd.invoice_cur_id=cm_invoice.cur_id
    and iis.is_active = 'Y'
    and gmr.is_deleted = 'N'
    and nvl(ivd.vat_amount_in_inv_cur,0)<>0
@@ -226,23 +239,14 @@ select akc.corporate_id,
        cm_pay.cur_id exposure_cur_id,
        cm_pay.cur_code exposure_currency,
        pfd.as_of_date trade_date, --pcm.issue_date trade_date,
-       /* pkg_general.f_get_converted_currency_amt(akc.corporate_id,
-                                                                     cm_base.cur_id,
-                                                                     cm_pay.cur_id,
-                                                                     pfd.as_of_date,
-                                                                     1) fx_rate,*/
-       (case
-         when pffxd.fx_rate_type = 'Fixed' then
-          pffxd.fixed_fx_rate
-         else
-          pfd.fx_rate
-       end) fx_rate,
+       pfd.fx_rate fx_rate,
        pcm.contract_ref_no,
        '' invoice_ref_no,
        '' parent_invoice_no,
        pcm.contract_ref_no || ' - ' || pcdi.delivery_item_no delivery_item_ref_no,
        pcm.contract_ref_no || ' Item No. ' || pcdi.delivery_item_no contract_item_ref_no,
-       gmr.gmr_ref_no gmr_ref_no,
+      -- gmr.gmr_ref_no gmr_ref_no,
+       nvl(gmr.gmr_ref_no,pfd.allocated_gmr_ref_no)gmr_ref_no,
        aml.attribute_name element_name,
        null currency_pair,
        pcdi.payment_due_date expected_payment_due_date,
@@ -283,28 +287,7 @@ select akc.corporate_id,
        pum.price_unit_id,
        pum.price_unit_name price_unit,
        null payable_receivable,
-       (case
-         when pfd.hedge_amount is null then
-          decode(pcm.purchase_sales, 'P', -1, 'S', 1) *
-          ((nvl(pfd.user_price, 0) + nvl(pfd.adjustment_price, 0)) /
-           nvl(ppu.weight, 1)) *
-         /*(round(pkg_general.f_get_converted_currency_amt(akc.corporate_id,
-                                                                              ppu.cur_id,
-                                                                              cm_pay.cur_id,
-                                                                              pfd.as_of_date,
-                                                                              1),
-                                     5) **/
-          (case
-         when pffxd.fx_rate_type = 'Fixed' then
-          pffxd.fixed_fx_rate
-         else
-          pfd.fx_rate
-       end) *
-       pkg_general.f_get_converted_quantity(nvl(pdm_under.product_id,
-                                                pdm.product_id),
-                                            qum.qty_unit_id,
-                                            pum.weight_unit_id,
-                                            pofh.per_day_pricing_qty) else pfd.hedge_amount end) hedging_amount,
+       decode(pcm.purchase_sales, 'P', -1, 'S', 1) * nvl(pfd.hedge_amount,0) hedging_amount,
        '' cost_type,
        null effective_date,
        '' buy_sell,
@@ -312,7 +295,9 @@ select akc.corporate_id,
        pfd.fx_correction_date correction_date,
        null activity_type,
        null activity_date,
-       null cpname
+       null cpname,
+       null vat_cur_code,
+       null invoice_cur_code
   from pcdi_pc_delivery_item          pcdi,
        pcm_physical_contract_main     pcm,
        poch_price_opt_call_off_header poch,
@@ -338,8 +323,7 @@ select akc.corporate_id,
        cm_currency_master           cm_pay,
        v_ppu_pum                    ppu,
        pum_price_unit_master        pum,
-       qum_quantity_unit_master     qum,
-       pffxd_phy_formula_fx_details pffxd
+       qum_quantity_unit_master     qum
  where pcdi.internal_contract_ref_no = pcm.internal_contract_ref_no
    and pcdi.pcdi_id = poch.pcdi_id
    and poch.is_free_metal_pricing = 'Y'
@@ -353,11 +337,7 @@ select akc.corporate_id,
    and pocd.pcbpd_id = pcbpd.pcbpd_id
    and pcbpd.pcbpd_id = ppfh.pcbpd_id(+)
    and ppfh.ppfh_id = pfqpp.ppfh_id(+)
-      --and nvl(pfqpp.is_qp_any_day_basis, 'N') <> 'Y'
-   and pofh.internal_gmr_ref_no = gmr.internal_gmr_ref_no(+)
-      /* and pcpch.internal_contract_ref_no = pcm.internal_contract_ref_no
-                                 and nvl(pcpch.payable_type, 'Payable') = 'Payable'
-                                 and poch.element_id = pcpch.element_id*/
+   and pofh.internal_gmr_ref_no = gmr.internal_gmr_ref_no(+)     
    and pcm.corporate_id = akc.corporate_id
    and pcm.trader_id = akcu.user_id(+)
    and akcu.gabid = gab.gabid
@@ -372,8 +352,7 @@ select akc.corporate_id,
    and ppu.price_unit_id = pum.price_unit_id(+)
    and pocd.qty_to_be_fixed_unit_id = qum.qty_unit_id
    and pcbpd.price_basis <> 'Fixed'
-   and pcm.contract_type = 'CONCENTRATES'
-      -- and pcm.approval_status = 'Approved'
+   and pcm.contract_type = 'CONCENTRATES' 
    and (case when pcm.is_tolling_contract = 'Y' then
         nvl(pcm.approval_status, 'Approved') else pcm.approval_status end) =
        'Approved'
@@ -389,12 +368,9 @@ select akc.corporate_id,
    and pfqpp.is_active(+) = 'Y'
    and pym.is_active = 'Y'
    and pym.is_deleted = 'N'
-   and pcbpd.pffxd_id = pffxd.pffxd_id -- Newly Added
-   and pffxd.is_active = 'Y' -- Newly Added
+   and pfd.hedge_amount is not null
    and nvl(pfd.is_hedge_correction, 'N') = 'N'
     and nvl(pfd.is_cancel,'N')='N'
---  and akc.corporate_id = '{?CorporateID}'
---  and to_char(pfd.as_of_date, 'dd-Mon-yyyy') = '{?AsOfDate}'
 union all
 select akc.corporate_id,
        akc.corporate_name,
@@ -419,18 +395,14 @@ select akc.corporate_id,
        cm_pay.cur_id exposure_cur_id,
        cm_pay.cur_code exposure_currency,
        pfd.as_of_date trade_date,
-       (case
-         when pffxd.fx_rate_type = 'Fixed' then
-          pffxd.fixed_fx_rate
-         else
-          pfd.fx_rate
-       end) fx_rate,
+       pfd.fx_rate fx_rate,
        pcm.contract_ref_no,
        null invoice_ref_no,
        null parent_invoice_no,
        pcm.contract_ref_no || ' - ' || pcdi.delivery_item_no delivery_item_ref_no,
        pcm.contract_ref_no || ' Item No. ' || pcdi.delivery_item_no contract_item_ref_no,
-       gmr.gmr_ref_no gmr_ref_no,
+       --gmr.gmr_ref_no gmr_ref_no,
+       nvl(gmr.gmr_ref_no,pfd.allocated_gmr_ref_no) gmr_ref_no, 
        aml.attribute_name element_name,
        null currency_pair,
        pcdi.payment_due_date expected_payment_due_date,
@@ -471,7 +443,7 @@ select akc.corporate_id,
        pum.price_unit_id,
        pum.price_unit_name price_unit,
        null payable_receivable,
-       pfd.hedge_amount hedging_amount,
+       decode(pcm.purchase_sales, 'P', -1, 'S', 1) * nvl(pfd.hedge_amount,0) hedging_amount,
        '' cost_type,
        null effective_date,
        '' buy_sell,
@@ -479,15 +451,16 @@ select akc.corporate_id,
        pfd.fx_correction_date correction_date,
        axs.action_id activity_type,
        axs.eff_date activity_date,
-       phd.companyname cpname
+       phd.companyname cpname,
+       null vat_cur_code,
+       null invoice_cur_code
   from pcdi_pc_delivery_item          pcdi,
        pcm_physical_contract_main     pcm,
        poch_price_opt_call_off_header poch,
        pocd_price_option_calloff_dtls pocd,
        pofh_price_opt_fixation_header pofh,
        pfd_price_fixation_details     pfd,
-       pcbpd_pc_base_price_detail     pcbpd,
-       pffxd_phy_formula_fx_details   pffxd, -- Newly Added
+       pcbpd_pc_base_price_detail     pcbpd, 
        ppfh_phy_price_formula_header  ppfh,
        pfqpp_phy_formula_qp_pricing   pfqpp,
        gmr_goods_movement_record      gmr,
@@ -512,12 +485,11 @@ select akc.corporate_id,
    and pocd.pocd_id = pofh.pocd_id(+)
    and pofh.pofh_id = pfd.pofh_id
    and pfd.is_active = 'Y'
-   and pocd.pcbpd_id = pcbpd.pcbpd_id
-   and pcbpd.pffxd_id = pffxd.pffxd_id -- Newly Added
-   and pffxd.is_active = 'Y' -- Newly Added
+   and pocd.pcbpd_id = pcbpd.pcbpd_id 
    and pcbpd.pcbpd_id = ppfh.pcbpd_id(+)
    and ppfh.ppfh_id = pfqpp.ppfh_id(+)
-   and pcm.internal_contract_ref_no = gmr.internal_contract_ref_no(+)
+  -- and pcm.internal_contract_ref_no = gmr.internal_contract_ref_no(+)
+   and pofh.internal_gmr_ref_no =gmr.internal_gmr_ref_no(+)
    and pcm.corporate_id = akc.corporate_id
    and pcm.trader_id = akcu.user_id(+)
    and akcu.gabid = gab.gabid
@@ -534,7 +506,6 @@ select akc.corporate_id,
    and pfd.hedge_correction_action_ref_no = axs.internal_action_ref_no
    and pcm.cp_id = phd.profileid
    and pcbpd.price_basis <> 'Fixed'
-      --and pcm.approval_status = 'Approved'
    and pcpd.input_output = 'Input'
    and pcdi.is_active = 'Y'
    and pcm.is_active = 'Y'
@@ -548,10 +519,9 @@ select akc.corporate_id,
    and pfqpp.is_active(+) = 'Y'
    and pym.is_active = 'Y'
    and pym.is_deleted = 'N'
+   and pfd.hedge_amount is not null
    and pfd.is_hedge_correction = 'Y'
    and nvl(pfd.is_cancel,'N')='N'
--- and akc.corporate_id = '{?CorporateID}'
--- and to_char(pfd.as_of_date, 'dd-Mon-yyyy') = '{?AsOfDate}'
 union all
 select akc.corporate_id,
        akc.corporate_name,
@@ -571,18 +541,13 @@ select akc.corporate_id,
        cm_pay.cur_id exposure_cur_id,
        cm_pay.cur_code exposure_currency,
        pfd.as_of_date trade_date,
-       (case
-         when pffxd.fx_rate_type = 'Fixed' then
-          pffxd.fixed_fx_rate
-         else
-          pfd.fx_rate
-       end) fx_rate,
+       pfd.fx_rate fx_rate,
        pcm.contract_ref_no,
        null invoice_ref_no,
        null parent_invoice_no,
        pcm.contract_ref_no || ' - ' || pcdi.delivery_item_no delivery_item_ref_no,
        pcm.contract_ref_no || ' Item No. ' || pcdi.delivery_item_no contract_item_ref_no,
-       gmr.gmr_ref_no gmr_ref_no,
+       nvl(gmr.gmr_ref_no,pfd.allocated_gmr_ref_no)gmr_ref_no,
        aml.attribute_name  element_name,
        null currency_pair,
        pcdi.payment_due_date expected_payment_due_date,
@@ -623,7 +588,7 @@ select akc.corporate_id,
        pum.price_unit_id,
        pum.price_unit_name price_unit,
        null payable_receivable,
-       pfd.hedge_amount hedging_amount,
+       decode(pcm.purchase_sales, 'P', -1, 'S', 1) * nvl(pfd.hedge_amount,0) hedging_amount,
        '' cost_type,
        null effective_date,
        '' buy_sell,
@@ -631,15 +596,16 @@ select akc.corporate_id,
        pfd.fx_correction_date correction_date,
        axs.action_id activity_type,
        axs.eff_date activity_date,
-       phd.companyname cpname
+       phd.companyname cpname,
+       null vat_cur_code,
+       null invoice_cur_code
   from pcdi_pc_delivery_item          pcdi,
        pcm_physical_contract_main     pcm,
        poch_price_opt_call_off_header poch,
        pocd_price_option_calloff_dtls pocd,
        pofh_price_opt_fixation_header pofh,
        pfd_price_fixation_details     pfd,
-       pcbpd_pc_base_price_detail     pcbpd,
-       pffxd_phy_formula_fx_details   pffxd, -- Newly Added
+       pcbpd_pc_base_price_detail     pcbpd,      
        ppfh_phy_price_formula_header  ppfh,
        pfqpp_phy_formula_qp_pricing   pfqpp,
        gmr_goods_movement_record      gmr,
@@ -664,12 +630,11 @@ select akc.corporate_id,
    and pocd.pocd_id = pofh.pocd_id(+)
    and pofh.pofh_id = pfd.pofh_id
    --and pfd.is_active = 'Y'
-   and pocd.pcbpd_id = pcbpd.pcbpd_id
-   and pcbpd.pffxd_id = pffxd.pffxd_id -- Newly Added
-   and pffxd.is_active = 'Y' -- Newly Added
+   and pocd.pcbpd_id = pcbpd.pcbpd_id 
    and pcbpd.pcbpd_id = ppfh.pcbpd_id(+)
    and ppfh.ppfh_id = pfqpp.ppfh_id(+)
-   and pcm.internal_contract_ref_no = gmr.internal_contract_ref_no(+)
+  -- and pcm.internal_contract_ref_no = gmr.internal_contract_ref_no(+)
+   and pofh.internal_gmr_ref_no=gmr.internal_gmr_ref_no(+)
    and pcm.corporate_id = akc.corporate_id
    and pcm.trader_id = akcu.user_id(+)
    and akcu.gabid = gab.gabid
@@ -685,8 +650,7 @@ select akc.corporate_id,
    and pocd.qty_to_be_fixed_unit_id = qum.qty_unit_id
    and pfd.hedge_correction_action_ref_no = axs.internal_action_ref_no
    and pcm.cp_id = phd.profileid
-   and pcbpd.price_basis <> 'Fixed'
-      --and pcm.approval_status = 'Approved'
+   and pcbpd.price_basis <> 'Fixed' 
    and pcpd.input_output = 'Input'
    and pcdi.is_active = 'Y'
    and pcm.is_active = 'Y'
@@ -702,5 +666,4 @@ select akc.corporate_id,
    and pym.is_deleted = 'N'
    and pfd.is_cancel='Y'
 /
--- and akc.corporate_id = '{?CorporateID}'
--- and to_char(pfd.as_of_date, 'dd-Mon-yyyy') = '{?AsOfDate}'
+
