52b1f99aecf7022c20a26e7b0e972eb2763959cf
rajeshwar.reddy <rajeshwar.reddy@ekaplus.com>
2012-11-17 11:29:48 +0000

    AssayListing Performance.

---
+++ DBScripts/ObjectsScripts/object_script_351.sql
@@ -0,0 +1,5 @@
+
+ALTER TABLE ash_assay_header ADD(dry_weight NUMBER(25,10));
+ALTER TABLE ash_assay_header ADD(wet_weight NUMBER(25,10));
+ALTER TABLE ash_assay_header ADD(qty_unit VARCHAR2(30 CHAR));
+ALTER TABLE ash_assay_header ADD(wnsrefno VARCHAR2(30 CHAR));
