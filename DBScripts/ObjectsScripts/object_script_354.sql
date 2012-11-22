906473d9756d4dcb904a6cc43ab781da2da31bfc
abhishek.vijay <abhishek.vijay@ekaplus.com>
2012-11-22 11:16:47 +0000

    wns document change

---
+++ DBScripts/ObjectsScripts/object_script_354.sql
@@ -0,0 +1,6 @@
+alter table WNS_ASSAY_D add BL_NO VARCHAR2 (50 Char);
+alter table WNS_ASSAY_D add BL_DATE DATE;
+alter table WNS_ASSAY_D add VESSEL_NAME VARCHAR2 (100 Char);
+alter table WNS_ASSAY_D add MODE_OF_TRANSPORT VARCHAR2 (15 Char);
+alter table WNS_ASSAY_D add CONTAINER_NO VARCHAR2 (500 Char);
+alter table WNS_ASSAY_D add CP_ADDRESS VARCHAR2 (500 Char);
