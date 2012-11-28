46c32a5252c2f970647ddd7a2b655afa799ba5a5
baijukaimala <baiju.kaimala@ekaplus.com>
2012-11-28 17:45:35 +0000

    Corrected Assay Type

--- DBScripts/DataCorrectionScripts/data_correction_script_23.sql
+++ DBScripts/DataCorrectionScripts/data_correction_script_23.sql
@@ -1,4 +1,4 @@
 update SPQL_STOCK_PAYABLE_QTY_LOG spql
 set spql.WEG_AVG_PRICING_ASSAY_ID = (select ash.ash_id from ASH_ASSAY_HEADER ash
 where ASH.PRICING_ASSAY_ASH_ID=SPQL.ASSAY_HEADER_ID
-and ASH.ASSAY_TYPE='Weighted Average Pricing Assay');
+and ASH.ASSAY_TYPE='Weighted Avg Pricing Assay');
