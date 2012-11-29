fc961d46828bef095de302fc79c3f4aaf47b8c31
suresh.gottipati <suresh.gottipati@ekaplus.com>
2012-11-29 12:30:00 +0000

    Bug Fixes

--- DBScripts/ObjectsScripts/eka_MV_BI_PHY_POSITION_MGR.sql
+++ DBScripts/ObjectsScripts/eka_MV_BI_PHY_POSITION_MGR.sql
@@ -2,7 +2,7 @@ drop materialized view MV_BI_PHY_POSITION_MGR;
 
 create materialized view MV_BI_PHY_POSITION_MGR
 refresh force on demand
-start with to_date('17-02-2012 20:24:59', 'dd-mm-yyyy hh24:mi:ss') next SYSDATE+60/1440 
+start with to_date('29-11-2012 20:24:59', 'dd-mm-yyyy hh24:mi:ss') next SYSDATE+60/1440 
 as
 select * from v_bi_phy_position_mgr;
 
