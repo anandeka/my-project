9e5a9780bd37fc120ae38ee346cf71ebf1f0498d
ramachandran.r <ramachandran.r@ekaplus.com>
2012-12-21 07:18:55 +0000

    Valuation Code

---
+++ DBScripts/DataCorrectionScripts/data_correction_script_31.sql
@@ -0,0 +1,67 @@
+
+update PCBPH_PC_BASE_PRICE_HEADER pcbph
+set pcbph.valuation_price_percentage=100;
+
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph
+set pcbph.valuation_price_percentage='100';
+
+
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph 
+   set pcbph.valuation_price_percentage =30
+   where PCBPH.PCBPH_ID=763;
+
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph 
+   set pcbph.valuation_price_percentage =30
+   where PCBPH.PCBPH_ID=775;
+
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph 
+   set pcbph.valuation_price_percentage =30
+   where PCBPH.PCBPH_ID=814;
+
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph 
+   set pcbph.valuation_price_percentage =92
+   where PCBPH.PCBPH_ID=1006;
+   
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph 
+   set pcbph.valuation_price_percentage =92
+   where PCBPH.PCBPH_ID=1007;
+
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph 
+   set pcbph.valuation_price_percentage =92
+   where PCBPH.PCBPH_ID=232;
+
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph 
+   set pcbph.valuation_price_percentage =92
+   where PCBPH.PCBPH_ID=399;
+
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph 
+   set pcbph.valuation_price_percentage =95
+   where PCBPH.PCBPH_ID=483;
+
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph 
+   set pcbph.valuation_price_percentage =98
+   where PCBPH.PCBPH_ID=1004;
+
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph 
+   set pcbph.valuation_price_percentage =98
+   where PCBPH.PCBPH_ID=1009;
+
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph 
+   set pcbph.valuation_price_percentage =98
+   where PCBPH.PCBPH_ID=161;
+
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph 
+   set pcbph.valuation_price_percentage =98
+   where PCBPH.PCBPH_ID=162;
+
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph 
+   set pcbph.valuation_price_percentage =98
+   where PCBPH.PCBPH_ID=233;
+
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph 
+   set pcbph.valuation_price_percentage =98
+   where PCBPH.PCBPH_ID=397;
+
+update PCBPHUL_PC_BASE_PRC_HEADER_UL pcbph 
+   set pcbph.valuation_price_percentage =98
+   where PCBPH.PCBPH_ID=485;
