fa64ccac30d3b803659b56db540aa686bf714b76
anupkumar.agarwal <anupkumar.agarwal@ekaplus.com>
2013-01-07 10:27:45 +0000

    Data correction for gmr shipped Qty In gmr And Agmr table For Warehouse receipt

--- DBScripts/DataCorrectionScripts/data_correction_script_34.sql
+++ DBScripts/DataCorrectionScripts/data_correction_script_34.sql
@@ -2,12 +2,12 @@
 
 UPDATE gmr_goods_movement_record gmr
    SET gmr.shipped_qty = gmr.qty
- WHERE gmr.gmr_latest_action_action_id in ('warehouseReceipt','releaseOrder')
+ WHERE gmr.gmr_latest_action_action_id in ('warehouseReceipt')
    AND NVL (gmr.is_internal_movement, 'N') = 'N'
    AND gmr.is_deleted = 'N';
    
 UPDATE AGMR_ACTION_GMR agmr
    SET agmr.shipped_qty = agmr.qty
- WHERE agmr.gmr_latest_action_action_id in ('warehouseReceipt','releaseOrder')
+ WHERE agmr.gmr_latest_action_action_id in ('warehouseReceipt')
    AND NVL (agmr.is_internal_movement, 'N') = 'N'
    AND agmr.is_deleted = 'N'
