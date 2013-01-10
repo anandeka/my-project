e233d052c523ff432ccf733c16b8f10d41f7249a
rajeshwar.reddy <rajeshwar.reddy@ekaplus.com>
2013-01-10 09:58:04 +0000

    Rounding Dry-wet ratio to 5 Decimals

---
+++ DBScripts/DataCorrectionScripts/data_correction_script_37.sql
@@ -0,0 +1,6 @@
+UPDATE asm_assay_sublot_mapping asm
+   SET asm.dry_wet_qty_ratio = ROUND((asm.dry_weight / asm.net_weight) * 100,5)
+ WHERE asm.ash_id IN (
+          SELECT ash.ash_id
+            FROM ash_assay_header ash, asm_assay_sublot_mapping asm
+           WHERE asm.ash_id = ash.ash_id);
