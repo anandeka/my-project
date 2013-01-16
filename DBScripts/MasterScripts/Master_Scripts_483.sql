c9b1216252eda49300f9866990b3f7c023cdb8b2
anupkumar.agarwal <anupkumar.agarwal@ekaplus.com>
2013-01-16 10:37:47 +0000

    Only Link Avaiable Will Be Under Gmr Operation For Wns

--- DBScripts/MasterScripts/Master_Scripts_483.sql
+++ DBScripts/MasterScripts/Master_Scripts_483.sql
@@ -1,4 +1,11 @@
 
-DELETE FROM gmc_grid_menu_configuration gmc
-      WHERE gmc.menu_id = '107';
+UPDATE gmc_grid_menu_configuration gmc
+   SET gmc.link_called = 'function(){loadWeighingAndSampling();}'
+ WHERE gmc.menu_id = '107';
 
+DELETE FROM gmc_grid_menu_configuration gmc
+      WHERE gmc.menu_id = 'LOGA-3';
+
+UPDATE gmc_grid_menu_configuration gmc
+   SET gmc.display_seq_no = '2'
+ WHERE gmc.menu_id = 'LOGA-2'
