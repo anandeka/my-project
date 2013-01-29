90d9c0647bff16aadf35e3a879715410d96f7035
b.ravikumar <b.ravikumar@ekaplus.com>
2013-01-29 09:16:58 +0000

    Bug #73245 Fix: filter added for successfully posted invoices.

---
+++ DBScripts/MasterScripts/Master_Scripts_486.sql
@@ -0,0 +1,10 @@
+Insert into SLV_STATIC_LIST_VALUE
+   (VALUE_ID, VALUE_TEXT)
+ Values
+   ('To Be Posted', 'To Be Posted');
+
+
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('PostingStatus', 'Posted Successfully', 'N', 8);
