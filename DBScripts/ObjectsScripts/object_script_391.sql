281bb0d85c6fad8997216a8e24fd45b085f37ef4
anandp <anand.hulbatte@ekaplus.com>
2013-02-15 14:33:49 +0000

    Increased the column size.

--- DBScripts/ObjectsScripts/object_script_391.sql
+++ DBScripts/ObjectsScripts/object_script_391.sql
@@ -1,2 +1,2 @@
-alter table is_d add our_person_incharge varchar2(20);
-alter table PFI_D add our_person_incharge varchar2(20);
+alter table is_d add our_person_incharge varchar2(100);
+alter table PFI_D add our_person_incharge varchar2(100);
