ba3470dd16b1586eb61d562ae990ac88e4c34a54
pradeep.kumar <pradeep.kumar@ekaplus.com>
2013-03-04 13:15:58 +0000

    -- # 75389 - Added Supplier Representative field on GMR creation page

---
+++ DBScripts/ObjectsScripts/object_script_400.sql
@@ -0,0 +1,16 @@
+alter table sd_shipment_detail add(supp_rep_id varchar2(100));
+
+alter table sdul_shipment_detail_ul add(supp_rep_id varchar2(100));
+
+alter table sad_shipment_advice add(supp_rep_id varchar2(100));
+
+alter table sadul_shipment_advice_ul add(supp_rep_id varchar2(100));
+
+alter table wrd_warehouse_receipt_detail add(supp_rep_id varchar2(100));
+
+alter table wrdul_warehouse_receipt_ul add(supp_rep_id varchar2(100));
+
+alter table rod_release_order_detail add(supp_rep_id varchar2(100));
+
+alter table rodul_release_order_detail_ul add(supp_rep_id varchar2(100));
+
