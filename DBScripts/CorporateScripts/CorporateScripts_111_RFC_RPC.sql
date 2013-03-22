ef381cb92d7c0b38221b32bf92594c3962916592
jana <janardhana@ekaplus.com>
2013-03-22 13:34:35 +0000

    New Sales Accrual Report Released

---
+++ DBScripts/CorporateScripts/CorporateScripts_111_RFC_RPC.sql
@@ -0,0 +1,197 @@
+-- RML
+
+Insert into RML_REPORT_MASTER_LIST
+   (REPORT_ID, SECTION_ID, REPORT_FILE_NAME, REPORT_DISPLAY_NAME, FEATURE_ID, 
+    REPORT_DISPLAY_NAME_DE, REPORT_DISPLAY_NAME_ES, ACTION_METHOD, REPORT_TYPE, IS_ACTIVE)
+ Values
+   ('365', '31', 'SalesAccrualReport.rpt', 'Sales Accrual Report', NULL, 
+    NULL, NULL, 'populateFilter', 'EOD', 'Y');
+
+-- REF
+
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('365', 'EXCEL', 'SalesAccrualReport_Excel.rpt');
+
+
+set define off;
+Insert into AMC_APP_MENU_CONFIGURATION
+   (MENU_ID, MENU_DISPLAY_NAME, DISPLAY_SEQ_NO, MENU_LEVEL_NO, LINK_CALLED, 
+    ICON_CLASS, MENU_PARENT_ID, ACL_ID, TAB_ID, FEATURE_ID, 
+    IS_DELETED)
+ Values
+   ('RPT-D365', 'Sales Accrual Report', 93, 5, '/EkaReport/CommonFilter.do?method=populateFilter&docType=EOM&ReportID=365&ReportName=SalesAccrualReport.rpt&ExportFormat=HTML&isEodReport=Y', 
+    NULL, 'RPT-D23', 'APP-ACL-N1305', 'Reports', 'APP-PFL-N-215', 
+    'N');
+
+
+-- RFC
+begin
+ for cc in (select *
+               from ak_corporate akc
+              where akc.is_internal_corporate = 'N') loop
+Insert into RFC_REPORT_FILTER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, LABEL_COLUMN_NUMBER, LABEL_ROW_NUMBER, 
+    LABEL, FIELD_ID, COLSPAN, IS_MANDATORY)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY03', 1, 3, 
+    'Warehouse Location', 'GFF1011', 1, 'Y');
+Insert into RFC_REPORT_FILTER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, LABEL_COLUMN_NUMBER, LABEL_ROW_NUMBER, 
+    LABEL, FIELD_ID, COLSPAN, IS_MANDATORY)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY01', 1, 1, 
+    'Year', 'GFF1012', 1, NULL);
+Insert into RFC_REPORT_FILTER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, LABEL_COLUMN_NUMBER, LABEL_ROW_NUMBER, 
+    LABEL, FIELD_ID, COLSPAN, IS_MANDATORY)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY02', 1, 2, 
+    'Month', 'GFF1012', 1, NULL);
+Insert into RFC_REPORT_FILTER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, LABEL_COLUMN_NUMBER, LABEL_ROW_NUMBER, 
+    LABEL, FIELD_ID, COLSPAN, IS_MANDATORY)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY04', 1, 4, 
+    'Supplier Type', 'GFF1012', 1, 'N');
+
+end loop;
+end;
+/
+-- RPC
+begin
+ for cc in (select *
+               from ak_corporate akc
+              where akc.is_internal_corporate = 'N') loop
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY02', 'RFP1065', '1');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY02', 'RFP1066', 'Yes');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY01', 'RFP1060', 'yearList');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY01', 'RFP1062', 'Yes');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY01', 'RFP1063', 'Filter');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY01', 'RFP1064', 'reportForm');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY01', 'RFP1065', '1');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY01', 'RFP1066', 'Yes');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY01', 'RFP1061', 'Year');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY02', 'RFP1060', 'MonthList');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY02', 'RFP1062', 'Yes');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY02', 'RFP1061', 'Month');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY02', 'RFP1063', 'Filter');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY02', 'RFP1064', 'reportForm');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY03', 'RFP1045', 'warehouseshed');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY03', 'RFP1046', 'WareHouse');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY03', 'RFP1047', 'No');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY03', 'RFP1048', 'Filter');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY03', 'RFP1049', 'reportForm');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY03', 'RFP1050', '1');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY03', 'RFP1053', 'Yes');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY03', 'RFP1051', 'multiple');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY03', 'RFP1052', 'WAREHOUSING');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY04', 'RFP1060', 'SupplierTypeList');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY04', 'RFP1061', 'SupplierType');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY04', 'RFP1062', 'No');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY04', 'RFP1063', 'Filter');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY04', 'RFP1064', 'reportForm');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY04', 'RFP1065', '1');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY04', 'RFP1066', 'Yes');
+Insert into RPC_RF_PARAMETER_CONFIG
+   (CORPORATE_ID, REPORT_ID, LABEL_ID, PARAMETER_ID, REPORT_PARAMETER_NAME)
+ Values
+   (cc.corporate_id, '365', 'RFC236PHY04', 'RFP1212', 'Yes');
+
+end loop;
+end;
+/
+
+
+commit;
