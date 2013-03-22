86e32d1055e921287102afe67dca9bc61d3ae49f
nishikant.gupta <nishikant.gupta@ekaplus.com>
2013-03-22 11:13:52 +0000

    Updated for filter.

---
+++ DBScripts/MasterScripts/Master_Scripts_530.sql
@@ -0,0 +1,8 @@
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('arrivedGMRSearch', 'Contract Ref No', 'N', 2);
+Insert into SLS_STATIC_LIST_SETUP
+   (LIST_TYPE, VALUE_ID, IS_DEFAULT, DISPLAY_ORDER)
+ Values
+   ('arrivedGMRSearch', 'GMR Ref No', 'N', 1);
