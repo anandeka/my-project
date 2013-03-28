9593d092c878797c1e0e5cdb14a48487093896e7
ramachandran.r <ramachandran.r@ekaplus.com>
2013-03-28 13:58:01 +0000

    Receive Material For Mine.

---
+++ DBScripts/MasterScripts/Master_Scripts_534.sql
@@ -0,0 +1,18 @@
+Insert into AKM_ACTION_REF_KEY_MASTER
+   (ACTION_KEY_ID, ACTION_KEY_DESC, VALIDATION_QUERY)
+ Values
+   ('RMMRefNo', 'RMM Ref No', 'SELECT COUNT(*) FROM   AXS_ACTION_SUMMARY axs WHERE  axs.action_ref_no = :pc_action_ref_no AND    axs.corporate_id = :pc_corporate_id');
+
+Insert into AXM_ACTION_MASTER
+   (ACTION_ID, ENTITY_ID, ACTION_NAME, IS_NEW_GMR_APPLICABLE, ACTION_DESC, 
+    IS_GENERATE_DOC_APPLICABLE, IS_REF_NO_GEN_APPLICABLE, IS_CONTINUOUS_MIDDLE_NO_REQ, IS_EVENT_PUBLISH_APPLICABLE)
+ Values
+   ('RECORD_MINING_OUTPUT', 'GMR Tolling ', 'Receive Material Mine', 'Y', 'Receive Material Mine Creation', 
+    'N', NULL, 'Y', 'N');
+    
+Insert into CAC_CORPORATE_ACTION_CONFIG
+   (ACTION_ID, IS_ACCRUAL_POSSIBLE, IS_ESTIMATE_POSSIBLE, EFF_DATE_FIELD, IS_DOC_APPLICABLE, 
+    GMR_STATUS_ID, SHIPMENT_STATUS, IS_AFLOAT, IS_INV_POSTING_REQD)
+ Values
+   ('RECORD_MINING_OUTPUT', 'N', 'N', 'activityDate', 'N', 
+    '2', 'In Warehouse', 'N', 'N');
