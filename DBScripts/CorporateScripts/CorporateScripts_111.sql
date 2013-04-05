828c687316d4dd6cc50e40f984a0728b9383c098
ramachandran.r <ramachandran.r@ekaplus.com>
2013-04-05 07:08:27 +0000

    Receive Material Mine for Cancellation.

---
+++ DBScripts/CorporateScripts/CorporateScripts_111.sql
@@ -0,0 +1,24 @@
+BEGIN
+   FOR cc IN (SELECT akc.corporate_id
+                FROM ak_corporate akc
+               WHERE akc.is_active = 'Y' AND akc.is_internal_corporate = 'N')
+   LOOP
+      INSERT INTO arf_action_ref_number_format
+                  (action_ref_number_format_id, action_key_id, corporate_id,
+                   prefix, middle_no_start_value, middle_no_last_used_value,
+                   suffix, VERSION, is_deleted
+                  )
+           VALUES ('ARF-CRMM-'||cc.corporate_id, 'CRMMRefNo', cc.corporate_id,
+                   'RMM-', 1, 1,
+                   '-'||cc.corporate_id, 1, 'N'
+                  );
+
+      INSERT INTO arfm_action_ref_no_mapping
+                  (action_ref_no_mapping_id, corporate_id, action_id,
+                   action_key_id, is_deleted
+                  )
+           VALUES ('ARFM-CRMM-'||cc.corporate_id, cc.corporate_id, 'RECORD_MINING_OUTPUT_CANCEL',
+                   'CRMMRefNo', 'N'
+                  );
+   END LOOP;
+END;
