60d42d3e7807a5d2038d017322513ff0c299c864
sriveenam <sriveena.mattaparthi@ekaplus.com>
2013-04-23 10:33:32 +0000

    bug fix 77822

---
+++ DBScripts/MasterScripts/Master_Scripts_558.sql
@@ -0,0 +1,21 @@
+ declare
+
+ cursor cr_csul is
+   SELECT cs.effective_date, t.internal_cost_ul_id
+  FROM (SELECT csul.effective_date,
+               pkg_general.f_is_date(csul.effective_date) is_date,
+               csul.internal_cost_ul_id, csul.internal_cost_id cs_id
+          FROM csul_cost_store_ul csul) t,
+       cs_cost_store cs
+ WHERE is_date = 'N' AND cs.internal_cost_id = t.cs_id;
+ 
+ begin
+
+  for rec_csul in cr_csul loop
+    update CSUL_COST_STORE_UL set EFFECTIVE_DATE = to_char(rec_csul.effective_date ,'dd-MON-yy') where INTERNAL_COST_UL_ID = rec_csul.internal_cost_ul_id;
+  end loop;
+end;
+
+
+
+delete from AXED_ACTION_ENTITY_DETAILS axed where AXED.ID in('3036','3035','922');
