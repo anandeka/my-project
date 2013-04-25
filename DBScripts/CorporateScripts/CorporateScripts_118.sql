beb8545be4494be43741cd94bf8d8a4273595345
rajuga <raju.ga@ekaplus.com>
2013-04-25 09:45:25 +0000

    Bug 77601 - Need to add extra text on each footer page below corporate address for Boliden Tara corporate

---
+++ DBScripts/CorporateScripts/CorporateScripts_118.sql
@@ -0,0 +1,6 @@
+UPDATE AK_CORPORATE_LOGO
+SET 
+    DIRECTORS_DISPLAY = 'Directors:  J.Moström (Chairman) (Swedish), M.Flynn (Managing  Director), T.Farrell, Y.Scannell',
+    COMP_REG_DISPLAY = 'Registered in Ireland 33148';
+    
+Commit;    
