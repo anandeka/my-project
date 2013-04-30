03c348fb613b8afa93c8186bf5e1963c0dc31f6e
suresh.gottipati <suresh.gottipati@ekaplus.com>
2013-04-30 07:47:07 +0000

    Bug Fixes

---
+++ DBScripts/EODScripts/136_DDL_EODDB_ALTER_SCRIPT.sql
@@ -0,0 +1,4 @@
+alter table  GRD_GOODS_RECORD_DETAIL  modify NO_OF_CONTAINERS number(9);
+alter table  ISR1_ISR_INVENTORY  modify NO_OF_CONTAINERS number(9);
+alter table  ISR2_ISR_INVOICE  modify NO_OF_CONTAINERS number(9);
+alter table  ISR_INTRASTAT_GRD  modify NO_OF_CONTAINERS number(9);
