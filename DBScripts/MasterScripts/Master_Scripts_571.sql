38b7e6216c34e351c1ccea58e22806250baeb4c9
ramachandran.r <ramachandran.r@ekaplus.com>
2013-05-08 14:49:17 +0000

    Auditlog bug fix

---
+++ DBScripts/MasterScripts/Master_Scripts_571.sql
@@ -0,0 +1,517 @@
+
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1104, 'shipmentDetail', 'isApplyContainerCharge', 'Is Apply Container Charge', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1105, 'shipmentDetail', 'landingStockArngType', 'Landing Stock Arng Type', '');
+
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1107, 'shipmentDetail', 'eta', 'ETA', '');   
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1108, 'shipmentDetail', 'loadinDate', 'Loading Date', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1109, 'shipmentDetail', 'carriersAgentsEndorsements', 'Carriers Agents Endorsements', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1110, 'shipmentDetail', 'comments', 'Comments', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1111, 'shipmentDetail', 'notes', 'Notes', '');
+   
+   
+
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1278, 'truckDetail', 'containerServiceType', 'Container Service Type', '');   
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1279, 'truckDetail', 'consigneeId', 'Consignee Name', ''); 
+
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1280, 'truckDetail', 'voyageNumber', 'Voyage Number', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1281, 'truckDetail', 'bookingRefNo', 'Booking Ref No', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1282, 'truckDetail', 'vesselVoyageName', 'Vessel Voyage Name', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1283, 'truckDetail', 'isApplyContainerCharge', 'Is Apply Container Charge', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1284, 'truckDetail', 'landingStockArngType', 'Landing Stock Arng Type', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1286, 'truckDetail', 'eta', 'ETA', '');   
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1287, 'truckDetail', 'loadinDate', 'Loading Date', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1288, 'truckDetail', 'carriersAgentsEndorsements', 'Carriers Agents Endorsements', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1289, 'truckDetail', 'comments', 'Comments', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1290, 'truckDetail', 'notes', 'Notes', '');
+   
+
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1378, 'airDetail', 'bookingRefNo', 'Booking Ref No', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1379, 'airDetail', 'voyageNumber', 'Voyage Number', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1380, 'airDetail', 'vesselVoyageName', 'Vessel Voyage Name', '');   
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1381, 'airDetail', 'isApplyContainerCharge', 'Is Apply Container Charge', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1382, 'airDetail', 'landingStockArngType', 'Landing Stock Arng Type', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1384, 'airDetail', 'eta', 'ETA', '');   
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1385, 'airDetail', 'loadinDate', 'Loading Date', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1386, 'airDetail', 'carriersAgentsEndorsements', 'Carriers Agents Endorsements', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1387, 'airDetail', 'comments', 'Comments', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1388, 'airDetail', 'notes', 'Notes', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1389, 'airDetail', 'agentsDataCode', 'Agents Data Code', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1390, 'airDetail', 'airportOfDepartureCode', 'Airport Of Departure Code', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1391, 'airDetail', 'airportOfDestinationCode', 'Airport Of Destination Code', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1392, 'airDetail', 'carriersAgentsEndorsements', 'Carriers Agents Endorsements', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1393, 'airDetail', 'dimensions', 'Dimensions', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1394, 'airDetail', 'handlingInstructions', 'Handling Instructions', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1395, 'airDetail', 'natureOfGoods', 'Nature Of Goods', '');
+   
+   
+
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1478, 'railDetail', 'bookingRefNo', 'Booking Ref No', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1479, 'railDetail', 'vesselVoyageName', 'Vessel Voyage Name', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1480, 'railDetail', 'voyageNumber', 'Voyage Number', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1481, 'railDetail', 'consigneeId', 'Consignee Name', '');   
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1482, 'railDetail', 'isApplyContainerCharge', 'Is Apply Container Charge', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1483, 'railDetail', 'landingStockArngType', 'Landing Stock Arng Type', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1485, 'railDetail', 'eta', 'ETA', '');   
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1486, 'railDetail', 'loadinDate', 'Loading Date', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1487, 'railDetail', 'carriersAgentsEndorsements', 'Carriers Agents Endorsements', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1488, 'railDetail', 'comments', 'Comments', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1489, 'railDetail', 'notes', 'Notes', '');
+      
+
+
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2080, 'shipmentAdvise', 'eta', 'ETA', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2081, 'shipmentAdvise', 'loadingDate', 'Loading Date', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2082, 'shipmentAdvise', 'isApplyContainerCharge', 'Is Apply Container Charge', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2083, 'shipmentAdvise', 'landingStockArngType', 'Landing Stock Arng Type', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2084, 'shipmentAdvise', 'carriersAgentsEndorsements', 'Carriers Agents Endorsements', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2085, 'shipmentAdvise', 'comments', 'Comments', '');
+
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2086, 'shipmentAdvise', 'notes', 'Notes', '');
+   
+
+
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2176, 'truckAdvice', 'listMetalStockDetailDO', 'Stock Name', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2177, 'truckAdvice', 'bookingRefNo', 'Booking Ref No', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2178, 'truckAdvice', 'vesselVoyageName', 'Vessel Voyage Name', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2179, 'truckAdvice', 'voyageNumber', 'Voyage Number', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2180, 'truckAdvice', 'eta', 'ETA', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2181, 'truckAdvice', 'loadingDate', 'Loading Date', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2182, 'truckAdvice', 'containerServiceType', 'Container Service Type', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2183, 'truckAdvice', 'consigneeId', 'Consignee Name', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2184, 'truckAdvice', 'isApplyContainerCharge', 'Is Apply Container Charge', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2185, 'truckAdvice', 'carriersAgentsEndorsements', 'Carriers Agents Endorsements', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2186, 'truckAdvice', 'comments', 'Comments', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2187, 'truckAdvice', 'notes', 'Notes', '');
+   
+
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2276, 'airAdvice', 'listMetalStockDetailDO', 'Stock Name', '');   
+
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2277, 'airAdvice', 'bookingRefNo', 'Booking Ref No', '');
+   
+
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2278, 'airAdvice', 'eta', 'ETA', '');  
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2279, 'airAdvice', 'loadingDate', 'Loading Date', '');
+   
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2280, 'airAdvice', 'vesselVoyageName', 'Vessel Voyage Name', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2281, 'airAdvice', 'voyageNumber', 'Voyage Number', ''); 
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2282, 'airAdvice', 'isApplyContainerCharge', 'Is Apply Container Charge', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2283, 'airAdvice', 'landingStockArngType', 'Landing Stock Arng Type', '');   
+  
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2284, 'airAdvice', 'carriersAgentsEndorsements', 'Carriers Agents Endorsements', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2285, 'airAdvice', 'comments', 'Comments', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2286, 'airAdvice', 'notes', 'Notes', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2287, 'airAdvice', 'agentsDataCode', 'Agents Data Code', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2288, 'airAdvice', 'airportOfDepartureCode', 'Airport Of Departure Code', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2289, 'airAdvice', 'airportOfDestinationCode', 'Airport Of Destination Code', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2290, 'airAdvice', 'carriersAgentsEndorsements', 'Carriers Agents Endorsements', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2291, 'airAdvice', 'dimensions', 'Dimensions', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2292, 'airAdvice', 'handlingInstructions', 'Handling Instructions', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2293, 'airAdvice', 'natureOfGoods', 'Nature Of Goods', ''); 
+
+
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1824, 'landingDetail', 'customsRefNo', 'Customs Ref No', ''); 
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1825, 'landingDetail', 'loadinDate', 'Loading Date', '');  
+
+
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1937, 'weightNote', 'contractQtyTolCheckReq', 'Contract Qty Check Req', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1938, 'weightNote', 'counterPartyId', 'Counter Party Name', '303');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1939, 'weightNote', 'isApplyContainerCharge', 'Is Apply Container Charge', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (1940, 'weightNote', 'isApplyFreightAllowance', 'Is Apply Freight Allowance', '');
+   
+
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2737, 'salesWeightNote', 'contractQtyTolCheckReq', 'Contract Qty Check Req', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2738, 'salesWeightNote', 'counterPartyId', 'Counter Party Name', '303');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2739, 'salesWeightNote', 'isApplyContainerCharge', 'Is Apply Container Charge', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2740, 'salesWeightNote', 'isApplyFreightAllowance', 'Is Apply Freight Allowance', '');
+   
+
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2569, 'releaseOrder', 'counterPartyId', 'Counter Party Name', '303');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2570, 'releaseOrder', 'listMetalStockDetailDO', 'Stock Name', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2571, 'releaseOrder', 'issueId', 'Issue Name', '');
+   
+Insert into AXED_ACTION_ENTITY_DETAILS
+   (ID, ACTION_ID, FIELD_NAME, LABEL, QUERY_REF_ID)
+ Values
+   (2572, 'releaseOrder', 'issuersAddress', 'Issuers Address', '');
+
+-- Delete AXED unnecessary entries
+
+DELETE FROM axed_action_entity_details axed
+      WHERE axed.ID IN ('2559')
+        AND axed.action_id = 'releaseOrder';
+        
+--  Updtae AXED ProfitCenterId
+
+update AXED_ACTION_ENTITY_DETAILS axed set AXED.QUERY_REF_ID = '303' where AXED.ID = '1418'; 
