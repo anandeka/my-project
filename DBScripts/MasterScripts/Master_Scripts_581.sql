7faa700fce2fcf649997a4689706cf67fbcd3ecb
ramachandran.r <ramachandran.r@ekaplus.com>
2013-05-24 13:03:56 +0000

    List Of GMR Consolidated Summary.

---
+++ DBScripts/MasterScripts/Master_Scripts_581.sql
@@ -0,0 +1,6 @@
+Insert into GMC_GRID_MENU_CONFIGURATION
+   (MENU_ID, GRID_ID, MENU_DISPLAY_NAME, DISPLAY_SEQ_NO, MENU_LEVEL_NO, 
+    FEATURE_ID, LINK_CALLED, ICON_CLASS, MENU_PARENT_ID, ACL_ID)
+ Values
+   ('113', 'LOG', 'GMR Details', 12, 2, 
+    'APP-PFL-N-182', 'function(){getConsolidatedSummary();}', NULL, '102', 'APP-ACL-N1062');
