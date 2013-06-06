f8bb97b5df6fa0b856c6b0eff66d460e93b18b19
srinivasanmurugesan <srinivasan.murugesan@ekaplus.com>
2013-06-06 14:19:48 +0000

    Not null constraint removed for Is_scheduled column.

--- DBScripts/ObjectsScripts/object_script_452.sql
+++ DBScripts/ObjectsScripts/object_script_452.sql
@@ -1,6 +1,6 @@
 
 -- column to hold the value as 'Y' when the price process is run from scheduler.
 ALTER TABLE ppl_price_process_list
- ADD (is_scheduled  CHAR(1 CHAR) DEFAULT 'N' NOT NULL);
+ ADD (is_scheduled  CHAR(1 CHAR) DEFAULT 'N');
  
  
