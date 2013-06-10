a37d19f1930262d119952de14febf63368849b90
ramachandran.r <ramachandran.r@ekaplus.com>
2013-06-10 07:21:19 +0000

    Trading Contract Template

---
+++ DBScripts/MasterScripts/Master_Scripts_591.sql
@@ -0,0 +1,20 @@
+
+UPDATE amc_app_menu_configuration
+   SET acl_id = NULL
+ WHERE menu_id = 'P91' AND menu_parent_id = 'P9'
+ 
+UPDATE amc_app_menu_configuration
+   SET acl_id = NULL
+ WHERE menu_id = 'P92' AND menu_parent_id = 'P9'
+ 
+UPDATE amc_app_menu_configuration
+   SET acl_id = NULL
+ WHERE menu_id = 'P93' AND menu_parent_id = 'P9'
+ 
+UPDATE amc_app_menu_configuration
+   SET acl_id = NULL
+ WHERE menu_id = 'P94' AND menu_parent_id = 'P9'
+ 
+UPDATE amc_app_menu_configuration
+   SET acl_id = NULL
+ WHERE menu_id = 'P95' AND menu_parent_id = 'P9'
