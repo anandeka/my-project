197ac436a48ccaae42cf0f1600b411f57b16b515
jana <janardhana@ekaplus.com>
2013-06-10 13:34:16 +0000

    price fix sqls

--- MBV_Report_Dev_Temp/Master_Scriptss.sql
+++ MBV_Report_Dev_Temp/Master_Scriptss.sql
@@ -73,3 +73,18 @@ Insert into REF_REPORTEXPORTFORMAT
 
 
 commit;
+
+
+Insert into RML_REPORT_MASTER_LIST
+   (REPORT_ID, SECTION_ID, REPORT_FILE_NAME, REPORT_DISPLAY_NAME, FEATURE_ID, 
+    REPORT_DISPLAY_NAME_DE, REPORT_DISPLAY_NAME_ES, ACTION_METHOD, REPORT_TYPE, IS_ACTIVE)
+ Values
+   ('374', '31', 'PriceFixationReport.rpt', 'Price Fixation Report', NULL, 
+    NULL, NULL, 'populateFilter', 'EOM', 'Y');
+
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('374', 'EXCEL', 'PriceFixationReport_Excel.rpt');    
+
+commit;
