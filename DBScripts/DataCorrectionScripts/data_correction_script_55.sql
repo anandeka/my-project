a84588669f4d065e02bd53e5fe0c618748981967
varaprasad.b <varaprasad.b@ekaplus.com>
2013-11-12 14:43:04 +0000

    updating pqcq where isDiductible is null --Rajeshwar

---
+++ DBScripts/DataCorrectionScripts/data_correction_script_55.sql
@@ -0,0 +1,13 @@
+-- updating pqcq where isDiductible is null
+
+UPDATE pqca_pq_chemical_attributes pqca
+   SET pqca.is_deductible = 'N'
+ WHERE pqca.pqca_id IN (
+          SELECT PQCA1.PQCA_ID
+            FROM ash_assay_header ash,
+                 asm_assay_sublot_mapping asm,
+                 pqca_pq_chemical_attributes pqca1
+           WHERE ash.ash_id = asm.ash_id
+             AND asm.asm_id = pqca1.asm_id
+             AND ash.is_active = 'Y'
+             and PQCA1.IS_DEDUCTIBLE is null)
