4783ee5e72022fa806bbc1203375a2f521f6da65
suresh.gottipati <suresh.gottipati@ekaplus.com>
2013-11-28 11:49:03 +0000

    Bug Fixes

---
+++ DBScripts/MasterScripts/Master_Scripts_668.sql
@@ -0,0 +1,88 @@
+SET DEFINE OFF;
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('378', 'EXCEL', 'MonthlyArrivalQtySummaryReport_Excel.rpt');
+   
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('379', 'EXCEL', 'MonthlyArrivalQtyDetailedReport_Excel.rpt');
+
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('380', 'EXCEL', 'MonthlyFeedQtySummaryReport_Excel.rpt');
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('381', 'EXCEL', 'MonthlyFeedQtyDetailedReport_Excel.rpt');
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('382', 'EXCEL', 'MonthlyClosingQtySummaryReport_Excel.rpt');
+   Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('383', 'EXCEL', 'MonthlyClosingQtyDetailedReport_Excel.rpt'); 
+
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('384', 'EXCEL', 'MonthlyArrivalAmountSummaryReport_Excel.rpt');
+   
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('385', 'EXCEL', 'MonthlyArrivalAmountDetailedReport_Excel.rpt');
+
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('386', 'EXCEL', 'MonthlyFeedAmountSummaryReport_Excel.rpt');
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('387', 'EXCEL', 'MonthlyFeedAmountDetailedReport_Excel.rpt');
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('388', 'EXCEL', 'MonthlyClosingAmountSummaryReport_Excel.rpt');
+   Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('389', 'EXCEL', 'MonthlyClosingAmountDetailedReport_Excel.rpt');    
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('390', 'EXCEL', 'MonthlyTCRCOtherChargesSummary_Excel.rpt');
+   Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('391', 'EXCEL', 'MonthlyTCRCOtherChargesDetailed_Excel.rpt');    
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('396', 'EXCEL', 'AccuralReconAmountYTDSummaryReport_Excel.rpt');
+   Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('397', 'EXCEL', 'AccuralReconAmountYTDDetailedReport.Excel.rpt');    
+Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('398', 'EXCEL', 'AccuralReconAmountMTDSummaryReport_Excel.rpt');
+   Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('399', 'EXCEL', 'AccuralReconAmountMTDDetailedReport_Excel.rpt');    
+ Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('376', 'EXCEL', 'MonthlyArrivalReport_Elements_Excel.rpt');  
+ Insert into REF_REPORTEXPORTFORMAT
+   (REPORT_ID, EXPORT_FORMAT, REPORT_FILE_NAME)
+ Values
+   ('377', 'EXCEL', 'MonthlyFeedConsumptionReport_Elements_Excel.rpt');         
+   
+COMMIT;
