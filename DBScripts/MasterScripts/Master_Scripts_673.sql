ee68f671c1beb24e456e59a1249b20ff7b13841a
suresh.gottipati <suresh.gottipati@ekaplus.com>
2013-12-10 11:24:45 +0000

    Bug Fixes

---
+++ DBScripts/MasterScripts/Master_Scripts_673.sql
@@ -0,0 +1,3 @@
+update RML_REPORT_MASTER_LIST rml set RML.REPORT_DISPLAY_NAME='Monthly TC/RC/Other Charges Detailed Report' where RML.REPORT_ID=391;
+update RML_REPORT_MASTER_LIST rml set RML.REPORT_DISPLAY_NAME='Monthly TC/RC/Other Charges Summary Report' where RML.REPORT_ID=390;
+commit;
