c41d15f700e274ad7062483a812b3b51f65d77e8
rajeshwar.reddy <rajeshwar.reddy@ekaplus.com>
2013-12-12 06:04:20 +0000

    updating isDisuctible flag in PQCA for all the assays including inactive records also.

--- DBScripts/DataCorrectionScripts/data_correction_script_55.sql
+++ DBScripts/DataCorrectionScripts/data_correction_script_55.sql
@@ -2,11 +2,4 @@
 
 UPDATE pqca_pq_chemical_attributes pqca
    SET pqca.is_deductible = 'N'
- WHERE pqca.pqca_id IN (
-          SELECT PQCA1.PQCA_ID
-            FROM ash_assay_header ash,
-                 asm_assay_sublot_mapping asm,
-                 pqca_pq_chemical_attributes pqca1
-           WHERE ash.ash_id = asm.ash_id
-             AND asm.asm_id = pqca1.asm_id
-            and PQCA1.IS_DEDUCTIBLE is null)
+ WHERE pqca.is_deductible IS NULL;
