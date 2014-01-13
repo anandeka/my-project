7788c9b1fb42dbf73fec40625bcab541f1d8d9ae
rajeshwar.reddy <rajeshwar.reddy@ekaplus.com>
2014-01-13 15:31:15 +0000

    Set isActive flag to 'Y' in AssaySublotMapping.

--- DBScripts/DataCorrectionScripts/data_correction_script_60.sql
+++ DBScripts/DataCorrectionScripts/data_correction_script_60.sql
@@ -1,8 +1,16 @@
 UPDATE asm_assay_sublot_mapping asm
    SET asm.is_active = 'Y'
- WHERE asm.ash_id IN (
-                 SELECT ash.ash_id
-                   FROM ash_assay_header ash,
-                        asm_assay_sublot_mapping asm_inner
-                  WHERE ash.ash_id = asm_inner.ash_id AND ASH.IS_ACTIVE='Y'
-                  and ASM_INNER.IS_ACTIVE is null)
+ WHERE exists (SELECT ash.ash_id
+                   FROM ash_assay_header ash
+                        --asm_assay_sublot_mapping asm_inner
+                  WHERE ash.ash_id = asm.ash_id AND ASH.IS_ACTIVE='Y')
+      and ASM.IS_ACTIVE is null;
+commit;
+UPDATE asm_assay_sublot_mapping asm
+   SET asm.is_active = 'N'
+ WHERE exists (SELECT ash.ash_id
+                   FROM ash_assay_header ash
+                        --asm_assay_sublot_mapping asm_inner
+                  WHERE ash.ash_id = asm.ash_id AND ASH.IS_ACTIVE='N')
+and ASM.IS_ACTIVE is null;                  
+commit;
