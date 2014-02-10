123819f6fdfef5e0be6b6d9ee636c45b13b66b44
suresh.gottipati <suresh.gottipati@ekaplus.com>
2014-02-10 09:32:39 +0000

    89539- Issue Fixed

--- DBScripts/EODScripts/EKA_Objects_pkg_phy_custom_reports.sql
+++ DBScripts/EODScripts/EKA_Objects_pkg_phy_custom_reports.sql
@@ -4474,7 +4474,27 @@ create or replace package body pkg_phy_custom_reports is
          dvj.period_type_name);
     
     end loop;
+ -- added Suresh
+  for cur_update in (select eod_eom.internal_derivative_ref_no,
+                            dt_ref.derivative_ref_no
+                       from eod_eom_derivative_journal    eod_eom,
+                            dt_derivative_trade@eka_appdb dt,
+                            dt_derivative_trade@eka_appdb dt_ref
+                      where eod_eom.internal_derivative_ref_no =
+                            dt.internal_derivative_ref_no
+                        and dt.underlying_instr_ref_no =
+                            dt_ref.internal_derivative_ref_no
+                        and eod_eom.dbd_id =pc_dbd_id
+                        and dt.underlying_instr_ref_no is not null)
   
+  loop
+    update eod_eom_derivative_journal eod_eom
+       set eod_eom.underlying_derivative_ref_no = cur_update.derivative_ref_no
+     where eod_eom.internal_derivative_ref_no =
+           cur_update.internal_derivative_ref_no
+       and eod_eom.dbd_id = pc_dbd_id;
+  end loop;
+commit;
   exception
     when others then
       vobj_error_log.extend;
