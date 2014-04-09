2a1d3ffc515fe6cffdbaa44e7c19cdd78586a67b
unknown <varaprasadb@VARAPRASADB.ekablr.com>
2014-04-09 15:16:54 +0530

    Test git

--- DBScripts/TataSteel/001_Master_Script_tata_Invoice_Document.sql
+++ DBScripts/TataSteel/001_Master_Script_tata_Invoice_Document.sql
@@ -2,3 +2,7 @@ update cdc_corporate_doc_config cdc
 set cdc.doc_rpt_file_name = 'TATA_InvoiceDocument.rpt'
 where cdc.doc_rpt_file_name = 'InvoiceDocument.rpt';
 commit;
+
+
+
+Testing commit through GIT
